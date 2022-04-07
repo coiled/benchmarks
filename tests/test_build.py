b92cbb14a03b4c8c7b1b8d53bc53aa4882989be7
James Bourbeau <jrbourbeau@gmail.com>
2022-04-07 11:30:44 -0500

    Remove test skips

--- tests/test_build.py
+++ tests/test_build.py
@@ -1,11 +1,8 @@
 import conda.cli.python_api as Conda
-import pytest
 import yaml
 
 
-@pytest.mark.skip
 def get_conda_list_dict():
-    """Get conda list packages and versions in a dictionary"""
     # Get conda list packages in a list, and strip headers
     conda_list = Conda.run_command(Conda.Commands.LIST)[0].split("\n")[3:-1]
     conda_list_dict = dict()
@@ -17,7 +14,6 @@ def get_conda_list_dict():
     return conda_list_dict
 
 
-@pytest.mark.skip
 def get_meta_packages_dict():
     """Get packages and versions specified on meta.yaml as a dictionary"""
     with open("continuous_integration/recipe/meta.yaml", "r") as f:
@@ -34,7 +30,6 @@ def get_meta_packages_dict():
     return meta_packages_dict
 
 
-@pytest.mark.skip
 def test_install_dist():
     """Test that versions of packages pinned in meta match the
     versions installed"""
