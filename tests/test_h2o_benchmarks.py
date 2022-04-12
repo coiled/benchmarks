5e2379d4eb150eebbb69969c849aea0b8abe6c73
James Bourbeau <jrbourbeau@gmail.com>
2022-04-12 15:08:03 -0500

    Run benchmarks on multiple data files

--- tests/test_h2o_benchmarks.py
+++ tests/test_h2o_benchmarks.py
@@ -32,9 +32,18 @@ def client(cluster):
         client.restart()
 
 
-@pytest.fixture(scope="module")
-def ddf():
+@pytest.fixture(
+    scope="module",
+    params=[
+        "s3://coiled-datasets/h2o-benchmark/N_1e7_K_1e2_single.csv",
+        "s3://coiled-datasets/h2o-benchmark/N_1e8_K_1e2_single.csv",
+        "s3://coiled-datasets/h2o-benchmark/N_1e9_K_1e2_single.csv",
+    ],
+    ids=["0.5 GB", "5 GB", "50 GB"],
+)
+def ddf(request):
     yield dd.read_csv(
+        request.param,
         "s3://coiled-datasets/h2o-benchmark/N_1e7_K_1e2_single.csv",
         dtype={
             "id1": "category",
