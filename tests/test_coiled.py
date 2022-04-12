a0401666e7ba2f7385088186e47b75744dad1b9a
David Chudzicki <david@coiled.io>
2022-04-13 00:35:14 +0200

    switch to v2 clusters (declarative)

--- tests/test_coiled.py
+++ tests/test_coiled.py
@@ -1,21 +1,20 @@
 import sys
 import uuid
 
-import coiled
 import dask.dataframe as dd
 import pandas as pd
+from coiled.v2 import Cluster
 from dask.distributed import Client
 
 SOFTWARE = f"dask-engineering/coiled_dist-py{sys.version_info[0]}{sys.version_info[1]}"
 
 
 def test_quickstart():
-    with coiled.Cluster(
+    with Cluster(
         software=SOFTWARE,
         name="nyc-quickstart_" + str(uuid.uuid4()),
         account="dask-engineering",
         n_workers=10,
-        backend_options={"spot": False},
     ) as cluster:
 
         with Client(cluster) as client:  # noqa F841
