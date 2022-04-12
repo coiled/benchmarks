1469f99e861d53c355a255e575959c7f47f7d9d8
ncclementi <natyclementi@gmail.com>
2022-04-12 13:09:31 -0400

    use cluster context manager

--- tests/test_coiled.py
+++ tests/test_coiled.py
@@ -10,28 +10,28 @@ SOFTWARE = os.environ["SOFTWARE_ENV"]
 
 
 def test_quickstart():
-    cluster = coiled.Cluster(
+    with coiled.Cluster(
         software=SOFTWARE,
         name="nyc-quickstart_" + str(uuid.uuid4()),
         account="dask-engineering",
         n_workers=10,
         backend_options={"spot": False},
-    )
+    ) as cluster:
 
-    with Client(cluster) as client:  # noqa F841
-        ddf = dd.read_csv(
-            "s3://nyc-tlc/trip data/yellow_tripdata_2019-*.csv",
-            dtype={
-                "payment_type": "UInt8",
-                "VendorID": "UInt8",
-                "passenger_count": "UInt8",
-                "RatecodeID": "UInt8",
-            },
-            storage_options={"anon": True},
-            blocksize="16 MiB",
-        ).persist()
+        with Client(cluster) as client:  # noqa F841
+            ddf = dd.read_csv(
+                "s3://nyc-tlc/trip data/yellow_tripdata_2019-*.csv",
+                dtype={
+                    "payment_type": "UInt8",
+                    "VendorID": "UInt8",
+                    "passenger_count": "UInt8",
+                    "RatecodeID": "UInt8",
+                },
+                storage_options={"anon": True},
+                blocksize="16 MiB",
+            ).persist()
 
-        result = ddf.groupby("passenger_count").tip_amount.mean().compute()
+            result = ddf.groupby("passenger_count").tip_amount.mean().compute()
 
-        assert isinstance(result, pandas.core.series.Series)
-        assert not result.empty
+            assert isinstance(result, pandas.core.series.Series)
+            assert not result.empty
