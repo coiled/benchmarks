65514010128ac5edd291c42601225ae71968e223
Naty Clementi <natyclementi@gmail.com>
2022-04-12 14:04:10 -0400

    Add Coiled nyc taxi quickstart  (#31)
    Co-authored-by: James Bourbeau <jrbourbeau@users.noreply.github.com>

---
+++ tests/test_coiled.py
@@ -0,0 +1,37 @@
+import sys
+import uuid
+
+import coiled
+import dask.dataframe as dd
+import pandas as pd
+from dask.distributed import Client
+
+SOFTWARE = f"dask-engineering/coiled_dist-py{sys.version_info[0]}{sys.version_info[1]}"
+
+
+def test_quickstart():
+    with coiled.Cluster(
+        software=SOFTWARE,
+        name="nyc-quickstart_" + str(uuid.uuid4()),
+        account="dask-engineering",
+        n_workers=10,
+        backend_options={"spot": False},
+    ) as cluster:
+
+        with Client(cluster) as client:  # noqa F841
+            ddf = dd.read_csv(
+                "s3://nyc-tlc/trip data/yellow_tripdata_2019-*.csv",
+                dtype={
+                    "payment_type": "UInt8",
+                    "VendorID": "UInt8",
+                    "passenger_count": "UInt8",
+                    "RatecodeID": "UInt8",
+                },
+                storage_options={"anon": True},
+                blocksize="16 MiB",
+            ).persist()
+
+            result = ddf.groupby("passenger_count").tip_amount.mean().compute()
+
+        assert isinstance(result, pd.Series)
+        assert not result.empty
