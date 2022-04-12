830564d0085110caeb117e13a1e0b0a6c245b104
ncclementi <natyclementi@gmail.com>
2022-04-12 11:49:23 -0400

    add quickstart test

---
+++ tests/test_quickstart.py
@@ -0,0 +1,36 @@
+import os
+import uuid
+
+import coiled
+import dask.dataframe as dd
+import pandas
+from dask.distributed import Client
+
+SOFTWARE = os.environ["SOFTWARE_ENV"]
+
+cluster = coiled.Cluster(
+    software=SOFTWARE,
+    name="nyc-quickstart_" + str(uuid.uuid4()),
+    account="dask-engineering",
+    n_workers=10,
+    backend_options={"spot": False},
+)
+
+client = Client(cluster)
+
+ddf = dd.read_csv(
+    "s3://nyc-tlc/trip data/yellow_tripdata_2019-*.csv",
+    dtype={
+        "payment_type": "UInt8",
+        "VendorID": "UInt8",
+        "passenger_count": "UInt8",
+        "RatecodeID": "UInt8",
+    },
+    storage_options={"anon": True},
+    blocksize="16 MiB",
+).persist()
+
+result = ddf.groupby("passenger_count").tip_amount.mean().compute()
+
+assert isinstance(result, pandas.core.series.Series)
+assert not result.empty
