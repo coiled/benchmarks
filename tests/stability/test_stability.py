7c905222441fdf983e8a10219b8a9380e08bd37a
Simon Perkins <simon.perkins@gmail.com>
2022-04-13 13:27:38 +0200

    Run basic stability test on coiled cluster

--- tests/stability/test_stability.py
+++ tests/stability/test_stability.py
@@ -1,12 +1,28 @@
-import dask
+from contextlib import ExitStack
+import uuid
+import sys
 
+import coiled
+import dask
+from dask.distributed import Client
+
+SOFTWARE = f"dask-engineering/coiled_dist-py{sys.version_info[0]}{sys.version_info[1]}"
 
 def test_shuffle_stability():
-    df = dask.datasets.timeseries(
-        start="2000-01-01", end="2000-12-31", freq="1s", partition_freq="1D"
-    )
+    with ExitStack() as stack:
+        cluster = stack.enter_context(coiled.Cluster(
+            software=SOFTWARE,
+            name="coiled-stability-tests_" + str(uuid.uuid4()),
+            account="dask-engineering",
+            n_workers=10,
+            backend_options={"spot": False}))
 
-    sdf = df.shuffle(on="x")
+        client = stack.enter_context(Client(cluster))  # noqa F841
+        df = dask.datasets.timeseries(
+            start="2000-01-01", end="2000-12-31", freq="1s", partition_freq="1D"
+        )
 
-    sdf_size = sdf.memory_usage(index=True).sum() / (1024.0**3)
-    assert sdf_size.compute() > 1.0  # 1.0 GB
+        sdf = df.shuffle(on="x")
+
+        sdf_size = sdf.memory_usage(index=True).sum() / (1024.0**3)
+        assert sdf_size.compute() > 1.0  # 1.0 GB
