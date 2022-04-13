d32359449d5c31fb4701567a294493e038e9a1b6
James Bourbeau <jrbourbeau@gmail.com>
2022-04-12 22:06:08 -0500

    Build latest Coiled software environment

--- ci/create_latest_runtime_meta.py
+++ ci/create_latest_runtime_meta.py
@@ -2,19 +2,11 @@ from __future__ import annotations
 
 import pathlib
 
-import click
 import yaml
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
-@click.command(context_settings={"ignore_unknown_options": True})
-@click.option(
-    "--output",
-    type=click.Path(dir_okay=False, writable=True, path_type=pathlib.Path),
-    required=True,
-    help="Output file",
-)
-def main(output):
+def main():
     """Get package version pins specified in `recipe/meta.yaml`"""
     env = Environment(
         loader=FileSystemLoader(pathlib.Path(__file__).parent.parent / "recipe"),
@@ -22,8 +14,17 @@ def main(output):
     )
     template = env.get_template("meta.yaml")
     meta = yaml.safe_load(template.render())
-    with open(output, "w") as f:
-        f.write("\n".join(meta["requirements"]["run"]))
+    requirements = meta["requirements"]["run"]
+
+    # File compatible with `mamba install --file <...>`
+    with open("latest.txt", "w") as f:
+        f.write("\n".join(requirements))
+
+    # File compatible with `mamba env create --file <...>`
+    env = {"channels": ["coiled", "conda-forge"]}
+    env["dependencies"] = requirements
+    with open("latest.yaml", "w") as f:
+        yaml.dump(env, f)
 
 
 if __name__ == "__main__":
