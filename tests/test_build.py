937ab3e402881ef0e76337bbcba20d1c335c326d
ncclementi <natyclementi@gmail.com>
2022-04-14 18:44:47 -0400

    Intentionally fail and try on PR

--- tests/test_build.py
+++ tests/test_build.py
@@ -38,6 +38,10 @@ def get_meta_specifiers() -> dict[str, SpecifierSet]:
     return meta_specifiers
 
 
+def test_bad():
+    assert False
+
+
 @pytest.mark.latest_runtime
 def test_install_dist():
     # Test that versions of packages installed are consistent with those
