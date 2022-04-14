5eb304f724640065775eb690cfaaea5a2b9aa8ea
James Bourbeau <jrbourbeau@gmail.com>
2022-04-13 20:50:28 -0500

    Use new fixtures

--- tests/stability/test_stability.py
+++ tests/stability/test_stability.py
@@ -1,28 +1,12 @@
-import sys
-import uuid
-
-import coiled
 import dask
-from dask.distributed import Client
-
-SOFTWARE = f"dask-engineering/coiled_dist-py{sys.version_info[0]}{sys.version_info[1]}"
 
 
-def test_shuffle_stability():
-    with coiled.Cluster(
-        software=SOFTWARE,
-        name="coiled-stability-tests_" + str(uuid.uuid4()),
-        account="dask-engineering",
-        n_workers=10,
-        backend_options={"spot": False},
-    ) as cluster:
+def test_shuffle_stability(small_client):
+    df = dask.datasets.timeseries(
+        start="2000-01-01", end="2000-12-31", freq="1s", partition_freq="1D"
+    )
 
-        with Client(cluster) as client:  # noqa F841
-            df = dask.datasets.timeseries(
-                start="2000-01-01", end="2000-12-31", freq="1s", partition_freq="1D"
-            )
+    sdf = df.shuffle(on="x")
 
-            sdf = df.shuffle(on="x")
-
-            sdf_size = sdf.memory_usage(index=True).sum() / (1024.0**3)
-            assert sdf_size.compute() > 1.0  # 1.0 GB
+    sdf_size = sdf.memory_usage(index=True).sum() / (1024.0**3)
+    assert sdf_size.compute() > 1.0  # 1.0 GB
