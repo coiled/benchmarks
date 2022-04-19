61ea711357145cd69f6287e5aa188c904fa01e6b
Simon Perkins <simon.perkins@gmail.com>
2022-04-19 11:57:05 +0200

    Remove memory check warnings

--- tests/test_shuffle.py
+++ tests/test_shuffle.py
@@ -1,6 +1,5 @@
 import os
 import uuid
-import warnings
 
 import dask
 import dask.dataframe as dd
@@ -56,28 +55,6 @@ def s3_stability_write_url(s3, s3_stability_url):
 
 @pytest.fixture
 def shuffle_dataset(small_client, s3_stability_url):
-    # NOTE(sjperkins)
-    # I think the dataset and worker memory limits are related for this test
-    # as the dataset is large enough to provoke some spilling when it is
-    # persisted. Warn if the worker memory limits change egregiously.
-    # Alternatively, a separate cluster fixture may be appropriate
-    # for shuffle tests.
-    workers = [w for w in small_client.scheduler_info()["workers"].values()]
-    mem_limit = [w["memory_limit"] for w in workers.values()]
-    ONE_GB = 1024.0**3
-
-    if any(wm < 7.5 * ONE_GB for wm in mem_limit):
-        warnings.warn("Worker memory limit < 7.5GB")
-
-    if any(wm > 8.5 * ONE_GB for wm in mem_limit):
-        warnings.warn("Worker memory limit > 8.5GB")
-
-    if sum(mem_limit) > 85 * ONE_GB:
-        warnings.warn("Total worker memory limit > 85GB")
-
-    if sum(mem_limit) < 75 * ONE_GB:
-        warnings.warn("Total worker memory limit < 75GB")
-
     df = dask.datasets.timeseries(
         start="2000-01-01", end="2000-12-31", freq="50ms", partition_freq="1D"
     )
@@ -88,8 +65,7 @@ def shuffle_dataset(small_client, s3_stability_url):
         overwrite=True,
         storage_options=storage_options,
     )
-    size = df.memory_usage(index=True).sum() / ONE_GB
-
+    size = df.memory_usage(index=True).sum() / (1024.0**3)
     size, _ = dask.compute(write, size)
 
     yield dd.read_parquet(f"s3://{s3_stability_url}", storage_options=storage_options)
