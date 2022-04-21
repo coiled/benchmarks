7073d6bc62b419738ef9aeddf3b2f450048b5563
Naty Clementi <natyclementi@gmail.com>
2022-04-21 16:02:02 -0400

    Switch to coiled-runtime default envs forn non-latest runs (#73)

--- conftest.py
+++ conftest.py
@@ -1,4 +1,7 @@
+import json
 import os
+import shlex
+import subprocess
 import sys
 import uuid
 
@@ -30,10 +33,23 @@ def pytest_collection_modifyitems(config, items):
 
 @pytest.fixture(scope="session")
 def software():
-    return os.environ.get(
-        "COILED_SOFTWARE_NAME",
-        f"dask-engineering/coiled_dist-py{sys.version_info[0]}{sys.version_info[1]}",
-    )
+    try:
+        return os.environ["COILED_SOFTWARE_NAME"]
+    except KeyError:
+        # Determine software environment from local `coiled-runtime` version (in installed)
+        out = subprocess.check_output(
+            shlex.split("conda list --json coiled-runtime"), text=True
+        ).rstrip()
+        runtime_info = json.loads(out)
+        if runtime_info:
+            version = runtime_info[0]["version"].replace(".", "-")
+            py_version = f"{sys.version_info[0]}{sys.version_info[1]}"
+            return f"dask-engineering/coiled-runtime-{version}-py{py_version}"
+        else:
+            raise RuntimeError(
+                "Must either specific `COILED_SOFTWARE_NAME` environment variable "
+                "or have `coiled-runtime` installed"
+            )
 
 
 @pytest.fixture(scope="module")
