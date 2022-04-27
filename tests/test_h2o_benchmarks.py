c0dc1f40faa86331bc90f604b20597ad6f6e157a
ncclementi <natyclementi@gmail.com>
2022-04-27 18:26:55 -0400

    fix typo

--- tests/test_h2o_benchmarks.py
+++ tests/test_h2o_benchmarks.py
@@ -14,8 +14,8 @@ import pytest
         # "s3://coiled-datasets/h2o-benchmark/N_1e8_K_1e2_single.csv",
         # "s3://coiled-datasets/h2o-benchmark/N_1e9_K_1e2_single.csv",
         "s3://coiled-datasets/h2o-benchmark/N_1e7_K_1e2_parquet/*.parquet",
-        # "s3://coiled-datasets/h2o-benchmark/N_1e8_K_1e2_parquet/*.parquet",
-        # "s3://coiled-datasets/h2o-benchmark/N_1e9_K_1e2_parquet/*.parquet",
+        "s3://coiled-datasets/h2o-benchmark/N_1e8_K_1e2_parquet/*.parquet",
+        "s3://coiled-datasets/h2o-benchmark/N_1e9_K_1e2_parquet/*.parquet",
     ],
     ids=[
         "0.5 GB (csv)",
