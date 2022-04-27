3144c5494949e0e90a60f4eb1c828ddaad3e932b
ncclementi <natyclementi@gmail.com>
2022-04-27 16:27:36 -0400

    test col pushdown

--- tests/test_h2o_benchmarks.py
+++ tests/test_h2o_benchmarks.py
@@ -3,8 +3,9 @@ h2o-ai benchmark groupby part running on coiled.
 """
 
 import dask.dataframe as dd
-import pandas as pd
 import pytest
+
+# import pandas as pd
 
 
 @pytest.fixture(
@@ -50,70 +51,75 @@ def ddf(request):
         )
 
 
+def test_q1_col(ddf, small_client):
+    ddf = ddf[["id1", "v1"]]
+    ddf.groupby("id1", dropna=False, observed=True).agg({"v1": "sum"}).compute()
+
+
 def test_q1(ddf, small_client):
     ddf.groupby("id1", dropna=False, observed=True).agg({"v1": "sum"}).compute()
 
 
-def test_q2(ddf, small_client):
-    (
-        ddf.groupby(["id1", "id2"], dropna=False, observed=True)
-        .agg({"v1": "sum"})
-        .compute()
-    )
+# def test_q2(ddf, small_client):
+#     (
+#         ddf.groupby(["id1", "id2"], dropna=False, observed=True)
+#         .agg({"v1": "sum"})
+#         .compute()
+#     )
 
 
-def test_q3(ddf, small_client):
-    (
-        ddf.groupby("id3", dropna=False, observed=True)
-        .agg({"v1": "sum", "v3": "mean"})
-        .compute()
-    )
+# def test_q3(ddf, small_client):
+#     (
+#         ddf.groupby("id3", dropna=False, observed=True)
+#         .agg({"v1": "sum", "v3": "mean"})
+#         .compute()
+#     )
 
 
-def test_q4(ddf, small_client):
-    (
-        ddf.groupby("id4", dropna=False, observed=True)
-        .agg({"v1": "mean", "v2": "mean", "v3": "mean"})
-        .compute()
-    )
+# def test_q4(ddf, small_client):
+#     (
+#         ddf.groupby("id4", dropna=False, observed=True)
+#         .agg({"v1": "mean", "v2": "mean", "v3": "mean"})
+#         .compute()
+#     )
 
 
-def test_q5(ddf, small_client):
-    (
-        ddf.groupby("id6", dropna=False, observed=True)
-        .agg({"v1": "sum", "v2": "sum", "v3": "sum"})
-        .compute()
-    )
+# def test_q5(ddf, small_client):
+#     (
+#         ddf.groupby("id6", dropna=False, observed=True)
+#         .agg({"v1": "sum", "v2": "sum", "v3": "sum"})
+#         .compute()
+#     )
 
 
-def test_q7(ddf, small_client):
-    (
-        ddf.groupby("id3", dropna=False, observed=True)
-        .agg({"v1": "max", "v2": "min"})
-        .assign(range_v1_v2=lambda x: x["v1"] - x["v2"])[["range_v1_v2"]]
-        .compute()
-    )
+# def test_q7(ddf, small_client):
+#     (
+#         ddf.groupby("id3", dropna=False, observed=True)
+#         .agg({"v1": "max", "v2": "min"})
+#         .assign(range_v1_v2=lambda x: x["v1"] - x["v2"])[["range_v1_v2"]]
+#         .compute()
+#     )
 
 
-def test_q8(ddf, small_client):
-    (
-        ddf[~ddf["v3"].isna()][["id6", "v3"]]
-        .groupby("id6", dropna=False, observed=True)
-        .apply(
-            lambda x: x.nlargest(2, columns="v3"),
-            meta={"id6": "Int64", "v3": "float64"},
-        )[["v3"]]
-        .compute()
-    )
+# def test_q8(ddf, small_client):
+#     (
+#         ddf[~ddf["v3"].isna()][["id6", "v3"]]
+#         .groupby("id6", dropna=False, observed=True)
+#         .apply(
+#             lambda x: x.nlargest(2, columns="v3"),
+#             meta={"id6": "Int64", "v3": "float64"},
+#         )[["v3"]]
+#         .compute()
+#     )
 
 
-def test_q9(ddf, small_client):
-    (
-        ddf[["id2", "id4", "v1", "v2"]]
-        .groupby(["id2", "id4"], dropna=False, observed=True)
-        .apply(
-            lambda x: pd.Series({"r2": x.corr()["v1"]["v2"] ** 2}),
-            meta={"r2": "float64"},
-        )
-        .compute()
-    )
+# def test_q9(ddf, small_client):
+#     (
+#         ddf[["id2", "id4", "v1", "v2"]]
+#         .groupby(["id2", "id4"], dropna=False, observed=True)
+#         .apply(
+#             lambda x: pd.Series({"r2": x.corr()["v1"]["v2"] ** 2}),
+#             meta={"r2": "float64"},
+#         )
+#         .compute()
+#     )
