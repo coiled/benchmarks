82af6bd4663a8b046a7aa67bfb3cfa819579964b
Simon Perkins <simon.perkins@gmail.com>
2022-04-28 11:43:40 +0200

    Split runtime, h2o and stability tests into separate jobs

--- ci/scripts/remove_coiled_env.sh
+++ ci/scripts/remove_coiled_env.sh
@@ -3,9 +3,8 @@ set -o nounset
 set -o xtrace
 
 # Clean up an Coiled software environments we created just for this CI build
-export COILED_SOFTWARE_NAME=${{ env.COILED_SOFTWARE_NAME }}
 coiled env delete $COILED_SOFTWARE_NAME
-if [[ ${{ steps.runtime_tests.outcome }} != 'success' ]]
+if [[ $OUTCOME != 'success' ]]
 then
 python -c "raise Exception('Tests failed. Please see the output from the previous step for more details.')"
 fi
