afc6d2504affc3d597e9c58616c026c6123fd66f
ncclementi <natyclementi@gmail.com>
2022-05-17 14:01:06 -0400

    revert changes fto get meta yaml

--- ci/create_latest_runtime_meta.py
+++ ci/create_latest_runtime_meta.py
@@ -6,7 +6,6 @@ import pathlib
 import shlex
 import subprocess
 import sys
-from collections import OrderedDict
 from distutils.util import strtobool
 
 import yaml
@@ -33,53 +32,29 @@ def main():
     requirements = meta["requirements"]["run"]
 
     # Ensure Python is pinned to X.Y.Z version currently being used
-    pre_release = strtobool(os.environ.get("PRE_RELEASE", "false"))
     python_version = ".".join(map(str, tuple(sys.version_info)[:3]))
     for idx, req in enumerate(requirements):
         package_name = Requirement(req).name
-        if package_name == "python" and not pre_release:
+        if package_name == "python":
             requirements[idx] = f"python =={python_version}"
 
     # Optionally use the development version of `dask` and `distributed`
     # from `dask/label/dev` conda channel
     upstream = strtobool(os.environ.get("TEST_UPSTREAM", "false"))
-    if upstream or pre_release:
+    if upstream:
         upstream_packages = {"dask", "distributed"}
         for idx, req in enumerate(requirements):
             package_name = Requirement(req).name
             if package_name in upstream_packages:
                 requirements[idx] = get_latest_conda_build(package_name)
 
-    if pre_release:
-        from yaml import CDumper as Dumper
-
-        def dict_representer(dumper, data):
-            return dumper.represent_dict(data.items())
-
-        Dumper.add_representer(OrderedDict, dict_representer)
-
-        meta_ord = OrderedDict()
-
-        meta_ord["package"] = meta["package"]
-        meta_ord["build"] = meta["build"]
-        meta_ord["requirement"] = meta["requirements"]
-        meta_ord["test"] = meta["test"]
-        meta_ord["about"] = meta["about"]
-        meta_ord["extra"] = meta["extra"]
-
-        meta_ord["package"]["version"] = os.environ.get("VERSION_SUFFIX")
-        os.mkdir("pre_release")
-        with open("pre_release/meta.yaml", "w") as f:
-            yaml.dump(meta_ord, f, Dumper=Dumper)
-
-    else:
-        # File compatible with `mamba env create --file <...>`
-        env = {
-            "channels": ["conda-forge"],
-            "dependencies": requirements,
-        }
-        with open("latest.yaml", "w") as f:
-            yaml.dump(env, f)
+    # File compatible with `mamba env create --file <...>`
+    env = {
+        "channels": ["conda-forge"],
+        "dependencies": requirements,
+    }
+    with open("latest.yaml", "w") as f:
+        yaml.dump(env, f)
 
 
 if __name__ == "__main__":
