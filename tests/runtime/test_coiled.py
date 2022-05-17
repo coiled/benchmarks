14ba89078383ffbf03ae67faff21a7c8cd2370e7
Naty Clementi <natyclementi@gmail.com>
2022-05-17 12:52:03 -0400

    Update csv path and add parquet nyc test (#119)

--- tests/runtime/test_coiled.py
+++ tests/runtime/test_coiled.py
@@ -2,9 +2,9 @@ import dask.dataframe as dd
 import pandas as pd
 
 
-def test_quickstart(small_client):
+def test_quickstart_csv(small_client):
     ddf = dd.read_csv(
-        "s3://nyc-tlc/trip data/yellow_tripdata_2019-*.csv",
+        "s3://nyc-tlc/csv_backup/yellow_tripdata_2019-*.csv",
         dtype={
             "payment_type": "UInt8",
             "VendorID": "UInt8",
@@ -13,6 +13,19 @@ def test_quickstart(small_client):
         },
         storage_options={"anon": True},
         blocksize="16 MiB",
+    ).persist()
+
+    result = ddf.groupby("passenger_count").tip_amount.mean().compute()
+
+    assert isinstance(result, pd.Series)
+    assert not result.empty
+
+
+def test_quickstart_parquet(small_client):
+    ddf = dd.read_parquet(
+        "s3://nyc-tlc/trip data/yellow_tripdata_2019-*.parquet",
+        columns=["passenger_count", "tip_amount"],
+        storage_options={"anon": True},
     ).persist()
 
     result = ddf.groupby("passenger_count").tip_amount.mean().compute()
