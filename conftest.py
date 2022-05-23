15c0f188b357babc501504bb40ee9ebd7a6ab5a0
Greg Hayes <hayesgb@gmail.com>
2022-05-22 21:37:14 -0500

    The call to get_software() in dask.config.set is not updating the specificed software in coiled.Cluster.  This means that if I set the environmental variable COILED_SOFTWARE_NAME, we still pull a docker image based on the coiled runtime

--- conftest.py
+++ conftest.py
@@ -38,7 +38,6 @@ def get_software():
     try:
         return os.environ["COILED_SOFTWARE_NAME"]
     except KeyError:
-        print("Got keyerror")
         # Determine software environment from local `coiled-runtime` version (in installed)
         out = subprocess.check_output(
             shlex.split("conda list --json coiled-runtime"), text=True
@@ -70,9 +69,9 @@ def small_cluster(request):
         name=f"{module}-{uuid.uuid4().hex[:8]}",
         n_workers=10,
         worker_memory="8 GiB",
-        # worker_vm_types=["m5.large"],
+        worker_vm_types=["m5.large"],
         scheduler_vm_types=["m5.large"],
-        # software=get_software()
+        software=get_software()
     ) as cluster:
         yield cluster
 
