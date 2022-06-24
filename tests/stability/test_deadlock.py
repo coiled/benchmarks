afc3347188a7bf7f4ece9eef5676872cc828e429
ncclementi <natyclementi@gmail.com>
2022-06-24 19:14:57 -0400

    reduce testing until temp

--- tests/stability/test_deadlock.py
+++ tests/stability/test_deadlock.py
@@ -14,34 +14,34 @@ from packaging.version import Version
 )
 def test_repeated_merge_spill(upload_cluster_dump, upload_performance_report):
     with coiled.v2.Cluster(
-        name=f"test_deadlock-{uuid.uuid4().hex}",
+        name=f"test_deadlock-{uuid.uuid4().hex[:8]}",
         n_workers=20,
         worker_vm_types=["t3.medium"],
     ) as cluster:
         with Client(cluster) as client:
             with upload_cluster_dump(client, cluster):
-                # with upload_performance_report():
-                raise Exception
-                ddf = dask.datasets.timeseries(
-                    "2020",
-                    "2025",
-                    partition_freq="2w",
-                )
-                ddf2 = dask.datasets.timeseries(
-                    "2020",
-                    "2023",
-                    partition_freq="2w",
-                )
+                with upload_performance_report():
+                    raise Exception
+                    ddf = dask.datasets.timeseries(
+                        "2020",
+                        "2025",
+                        partition_freq="2w",
+                    )
+                    ddf2 = dask.datasets.timeseries(
+                        "2020",
+                        "2023",
+                        partition_freq="2w",
+                    )
 
-                for _ in range(10):
-                    client.restart()
-                    fs = client.compute((ddf.x + ddf.y).mean())
+                    for _ in range(10):
+                        client.restart()
+                        fs = client.compute((ddf.x + ddf.y).mean())
 
-                    wait(fs, timeout=2 * 60)
-                    del fs
+                        wait(fs, timeout=2 * 60)
+                        del fs
 
-                    ddf3 = ddf.merge(ddf2)
-                    fs = client.compute((ddf3.x + ddf3.y).mean())
+                        ddf3 = ddf.merge(ddf2)
+                        fs = client.compute((ddf3.x + ddf3.y).mean())
 
-                    wait(fs, timeout=2 * 60)
-                    del fs
+                        wait(fs, timeout=2 * 60)
+                        del fs
