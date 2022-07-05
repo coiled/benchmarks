076cd0c650f081531a49fc108ecee1add5684ddc
Naty Clementi <natyclementi@gmail.com>
2022-07-05 15:03:18 -0400

    skip test until distributed PR 6637 is merged (#204)

--- tests/stability/test_deadlock.py
+++ tests/stability/test_deadlock.py
@@ -2,15 +2,16 @@ import uuid
 
 import coiled.v2
 import dask
-import distributed
 import pytest
 from distributed import Client, wait
-from packaging.version import Version
 
 
-@pytest.mark.skipif(
-    Version(distributed.__version__) < Version("2022.4.2"),
-    reason="https://github.com/dask/distributed/issues/6110",
+# @pytest.mark.skipif(
+#     Version(distributed.__version__) < Version("2022.4.2"),
+#     reason="https://github.com/dask/distributed/issues/6110",
+# )
+@pytest.mark.skip(
+    reason="Skip until https://github.com/dask/distributed/pull/6637 is merged"
 )
 def test_repeated_merge_spill(upload_cluster_dump):
     with coiled.v2.Cluster(
