8662c067981b80634cae8a877017925ea69dfd08
Ian Rose <ian@coiled.io>
2022-07-06 17:51:49 -0700

    Handle missing data a bit better

--- dashboard.py
+++ dashboard.py
@@ -34,15 +34,16 @@ source = get_test_source()
 
 
 def make_timeseries(originalname, df, field):
+    kwargs = {}
+    if len(df.name.unique()) > 1:
+        kwargs["color"] = altair.Color("name:N")
     return (
         altair.Chart(df, width=512, height=256)
         .mark_line()
         .encode(
             x=altair.X("start:T"),
             y=altair.Y(f"{field}:Q"),
-            color=altair.Color("name:N")
-            if len(df.name.unique()) > 1
-            else altair.Undefined,
+            **kwargs,
         )
         .properties(title=originalname)
     )
@@ -51,21 +52,23 @@ def make_timeseries(originalname, df, field):
 def make_test_report(originalname, df):
     fields = {"duration": "Wall Clock"}
     tabs = []
-    print(originalname)
     for field, label in fields.items():
+        df = df[~df[field].isna()]
+        if not len(df):
+            continue
         chart = make_timeseries(originalname, df, field)
         tabs.append((label, chart))
 
     if originalname in source:
-        code = panel.pane.Markdown(f"```python\n{source[originalname]}\n```")
+        code = panel.pane.Markdown(f"```python\n{source[originalname]}\n```", width=512)
         tabs.append(("Source", code))
-    return panel.Tabs(*tabs)
+    return panel.Tabs(*tabs, margin=12, width=600)
 
 
 if __name__ == "__main__":
     df = pandas.read_sql_table("test_run", engine)
     grouped = df.groupby("originalname")
     panes = [make_test_report(name, grouped.get_group(name)) for name in grouped.groups]
-    flex = panel.FlexBox(*panes)
+    flex = panel.FlexBox(*panes, align_items="start", justify_content="space-between")
 
     flex.save("benchmarks.html", resources=INLINE)
