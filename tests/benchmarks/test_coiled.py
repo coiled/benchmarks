6b4f2bb7f6a065f20e44a7b5fc940365e6f04c9e
Ian Rose <ian@coiled.io>
2022-07-18 17:11:58 -0700

    Remove test tests

--- tests/benchmarks/test_coiled.py
+++ tests/benchmarks/test_coiled.py
@@ -1,7 +1,5 @@
 import uuid
 
-import dask.array as da
-import distributed
 import pytest
 from coiled import Cluster
 
@@ -15,24 +13,3 @@ def test_default_cluster_spinup_time(request):
 
     with Cluster(name=f"{request.node.originalname}-{uuid.uuid4().hex[:8]}"):
         pass
-
-
-@pytest.mark.parametrize("offset", [1, 2, 3, 4, 5])
-def test_stuff(offset):
-    import random
-    import time
-
-    time.sleep(offset + random.uniform(-0.2, 0.2))
-
-
-@pytest.fixture
-def local_client(sample_memory):
-    with distributed.Client() as client:
-        with sample_memory(client):
-            yield client
-
-
-@pytest.mark.parametrize("n", [100, 1000, 10_000])
-def test_sample_memory(n, local_client):
-    x = da.random.uniform(size=(n, n))
-    (x + x.T).mean(axis=1).compute()
