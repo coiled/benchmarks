64cb0f06cd9f5cea05af9e4817a19208181be0c8
ncclementi <natyclementi@gmail.com>
2022-08-04 18:06:30 -0400

    update db on PR and make sleep obvious

--- tests/benchmarks/test_coiled.py
+++ tests/benchmarks/test_coiled.py
@@ -5,6 +5,7 @@ from coiled import Cluster
 
 
 def test_default_cluster_spinup_time(request):
-    time.sleep(10)
+    # make an obvious sleep
+    time.sleep(60)
     with Cluster(name=f"{request.node.originalname}-{uuid.uuid4().hex[:8]}"):
         pass
