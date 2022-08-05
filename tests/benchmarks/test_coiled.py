b777fd241bb95672160fcd289516821c292743ce
ncclementi <natyclementi@gmail.com>
2022-08-05 18:42:16 -0400

    remove sleep

--- tests/benchmarks/test_coiled.py
+++ tests/benchmarks/test_coiled.py
@@ -1,11 +1,8 @@
-import time
 import uuid
 
 from coiled import Cluster
 
 
 def test_default_cluster_spinup_time(request):
-    # make an obvious sleep
-    time.sleep(60)
     with Cluster(name=f"{request.node.originalname}-{uuid.uuid4().hex[:8]}"):
         pass
