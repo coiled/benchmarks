b37a1bb19aecaa8a639ff78dee754fdfb05be102
Gabe Joseph <gjoseph92@gmail.com>
2022-08-11 14:54:31 -0600

    scale tests to cluster size

--- tests/workloads/test_array.py
+++ tests/workloads/test_array.py
@@ -1,71 +1,126 @@
+from __future__ import annotations
+
 import xarray as xr
 import dask.array as da
 from dask.utils import format_bytes
 import numpy as np
-import distributed
-import coiled
+
+from ..utils_test import scaled_array_shape, wait, cluster_memory
 
 
-def wait(thing, client, timeout):
-    p = thing.persist()
-    try:
-        distributed.wait(p, timeout=timeout)
-        for f in client.futures_of(p):
-            if f.status in ("error", "cancelled"):
-                raise f.exception()
-    finally:
-        client.cancel(p)
-
-
-def test_anom_mean(test_name_uuid, upload_cluster_dump, sample_memory, benchmark_time):
-    # From https://github.com/dask/distributed/issues/2602#issuecomment-498718651
-    data = da.random.random((10000, 1000000), chunks=(1, 1000000))
-    arr = xr.DataArray(
-        data, dims=["time", "x"], coords={"day": ("time", np.arange(10000) % 100)}
-    )
+def print_size_info(memory: int, target_nbytes: int, *arrs: da.Array) -> None:
     print(
-        f"{format_bytes(data.nbytes)} - {data.npartitions} {format_bytes(data.blocks[0, 0].nbytes)} chunks"
+        f"Cluster memory: {format_bytes(memory)}, target data size: {format_bytes(target_nbytes)}"
     )
-    # 74.51 GiB - 10000 7.63 MiB chunks
+    for i, arr in enumerate(arrs, 1):
+        print(
+            f"Input {i}: {format_bytes(arr.nbytes)} - "
+            f"{arr.npartitions} {format_bytes(arr.blocks[(0,) * arr.ndim].nbytes)} chunks",
+        )
+
+
+def test_anom_mean(small_client):
+    # From https://github.com/dask/distributed/issues/2602#issuecomment-498718651
+
+    memory = cluster_memory(small_client)  # 76.66 GiB
+    target_nbytes = memory // 2
+    data = da.random.random(
+        scaled_array_shape(target_nbytes, ("x", "10MiB")), chunks=(1, "10MiB")
+    )
+    print_size_info(memory, target_nbytes, data)
+    # 38.32 GiB - 3925 10.00 MiB chunks
+
+    ngroups = data.shape[0] // 100
+    arr = xr.DataArray(
+        data,
+        dims=["time", "x"],
+        coords={"day": ("time", np.arange(data.shape[0]) % ngroups)},
+    )
 
     clim = arr.groupby("day").mean(dim="time")
     anom = arr.groupby("day") - clim
     anom_mean = anom.mean(dim="time")
 
-    with coiled.Cluster(
-        name=test_name_uuid,
-        n_workers=20,
-        worker_vm_types=["r5a.large"],  # 2 CPU, 16GiB
-        environ={"MALLOC_TRIM_THRESHOLD_": 0},
-        wait_for_workers=True,
-        package_sync=True,
-    ) as cluster:
-        with distributed.Client(cluster) as client, upload_cluster_dump(
-            client, cluster
-        ), sample_memory(client):
-            wait(anom_mean, client, 10 * 60)
+    wait(anom_mean, small_client, 10 * 60)
 
 
-def test_basic_sum(test_name_uuid, upload_cluster_dump, sample_memory, benchmark_time):
+def test_basic_sum(small_client):
     # From https://github.com/dask/distributed/pull/4864
 
-    data = da.zeros((12500000, 5000), chunks=(12500000, 1))
-    print(
-        f"{format_bytes(data.nbytes)} - {data.npartitions} {format_bytes(data.blocks[0, 0].nbytes)} chunks"
+    memory = cluster_memory(small_client)  # 76.66 GiB
+    target_nbytes = memory * 5
+    data = da.zeros(
+        scaled_array_shape(target_nbytes, ("100MiB", "x")), chunks=("100MiB", 1)
     )
-    # 465.66 GiB - 5000 95.37 MiB chunks
+    print_size_info(memory, target_nbytes, data)
+    # 383.20 GiB - 3924 100.00 MiB chunks
 
     result = da.sum(data, axis=1)
 
-    with coiled.Cluster(
-        name=test_name_uuid,
-        n_workers=4,
-        worker_vm_types=["t3.xlarge"],  # 4 CPU, 16GiB
-        environ={"MALLOC_TRIM_THRESHOLD_": 0},
-        wait_for_workers=True,
-        package_sync=True,
-    ) as cluster:
-        with distributed.Client(cluster) as client, upload_cluster_dump(
-            client, cluster
-        ), sample_memory(client):
-            wait(result, client, 10 * 60)
+    wait(result, small_client, 10 * 60)
+
+
+def test_climatcic_mean(small_client):
+    # From https://github.com/dask/distributed/issues/2602#issuecomment-535009454
+
+    memory = cluster_memory(small_client)  # 76.66 GiB
+    target_nbytes = memory * 2
+    chunks = (1, 1, 96, 21, 90, 144)
+    shape = (28, "x", 96, 21, 90, 144)
+    data = da.random.random(scaled_array_shape(target_nbytes, shape), chunks=chunks)
+    print_size_info(memory, target_nbytes, data)
+    # 152.62 GiB - 784 199.34 MiB chunks
+
+    array = xr.DataArray(
+        data,
+        dims=["ensemble", "init_date", "lat", "lead_time", "level", "lon"],
+        # coords={"init_date": pd.date_range(start="1960", periods=arr.shape[1])},
+        coords={"init_date": np.arange(data.shape[1]) % 10},
+    )
+    # arr_clim = array.groupby("init_date.month").mean(dim="init_date")
+    arr_clim = array.groupby("init_date").mean(dim="init_date")
+
+    wait(arr_clim, small_client, 10 * 60)
+
+
+def test_vorticity(small_client):
+    # From https://github.com/dask/distributed/issues/6571
+
+    memory = cluster_memory(small_client)  # 76.66 GiB
+    target_nbytes = int(memory * 0.85)
+    shape = scaled_array_shape(target_nbytes, (5000, 5000, "x"))
+
+    u = da.random.random(shape, chunks=(5000, 5000, 1))
+    v = da.random.random(shape, chunks=(5000, 5000, 1))
+    print_size_info(memory, target_nbytes, u, v)
+    # Input 1: 65.19 GiB - 350 190.73 MiB chunks
+    # Input 2: 65.19 GiB - 350 190.73 MiB chunks
+
+    dx = da.random.random((5001, 5000), chunks=(5001, 5000))
+    dy = da.random.random((5001, 5000), chunks=(5001, 5000))
+
+    def pad_rechunk(arr):
+        """
+        Pad a single element onto the end of arr, then merge the 1-element long chunk created back in.
+
+        This operation complicates each chain of the graph enough so that the scheduler no longer recognises the overall computation as blockwise,
+        but doesn't actually change the overall topology of the graph, or the number of chunks along any dimension of the array.
+
+        This is motivated by the padding operation we do in xGCM, see
+
+        https://xgcm.readthedocs.io/en/latest/grid_ufuncs.html#automatically-applying-boundary-conditions
+        https://github.com/xgcm/xgcm/blob/fe860f96bbaa7293142254f48663d71fb97a4f36/xgcm/grid_ufunc.py#L871
+        """
+
+        padded = da.pad(arr, pad_width=[(0, 1), (0, 0), (0, 0)], mode="wrap")
+        old_chunks = padded.chunks
+        new_chunks = list(old_chunks)
+        new_chunks[0] = 5001
+        rechunked = da.rechunk(padded, chunks=new_chunks)
+        return rechunked
+
+    up = pad_rechunk(u)
+    vp = pad_rechunk(v)
+    result = dx[..., None] * up - dy[..., None] * vp
+
+    wait(result, small_client, 10 * 60)
