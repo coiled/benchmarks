4970724f2f675af30fdcd71e92f95207e2d1a464
Ian Rose <ian@coiled.io>
2022-08-11 17:42:28 -0700

    Revert "Try triggering cluster dump", seems to have worked fine
    This reverts commit 7282b9936fe7816ea9f446bc90992406c4b44c8d.

--- tests/stability/test_array.py
+++ tests/stability/test_array.py
@@ -6,11 +6,6 @@ def test_rechunk_in_memory(small_client):
     x = da.random.random((50000, 50000))
     x.rechunk((50000, 20)).rechunk((20, 50000)).sum().compute()
 
-    def raiser():
-        raise RuntimeError("oops!")
-
-    small_client.run(raiser)
-
 
 @pytest.mark.skip(reason="this runs forever")
 def test_rechunk_out_of_memory(small_client):
