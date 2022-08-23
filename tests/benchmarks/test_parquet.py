e0a4d694488c41c97359f022a68ff39d2d03df05
Ian Rose <ian@coiled.io>
2022-08-22 17:54:35 -0700

    Initial plugin for measuring task durations for compute, transfer, disk spill, and deserialization

--- tests/benchmarks/test_parquet.py
+++ tests/benchmarks/test_parquet.py
@@ -26,12 +26,14 @@ def parquet_cluster():
 
 
 @pytest.fixture
-def parquet_client(parquet_cluster, sample_memory, benchmark_time):
+def parquet_client(
+    parquet_cluster, benchmark_task_durations, benchmark_memory, benchmark_time
+):
     with distributed.Client(parquet_cluster) as client:
         parquet_cluster.scale(N_WORKERS)
         client.wait_for_workers(N_WORKERS)
         client.restart()
-        with sample_memory(client), benchmark_time:
+        with benchmark_memory(client), benchmark_task_durations(client), benchmark_time:
             yield client
 
 
