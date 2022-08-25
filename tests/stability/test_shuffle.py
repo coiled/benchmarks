6c2665e84f9bcefd994a265df9b6bd79e70a650b
Ian Rose <ian@coiled.io>
2022-08-25 14:13:57 -0700

    Remove now-unneeded workaround

--- tests/stability/test_shuffle.py
+++ tests/stability/test_shuffle.py
@@ -32,10 +32,4 @@ def test_shuffle_parquet(small_client, s3_url, s3_storage_options):
     shuffled_url = s3_url + "/shuffled.parquet"
     df_shuffled = dd.read_parquet(dataset_url, storage_options=s3_storage_options)
     df_shuffled = df_shuffled.shuffle(on="x")
-    # Workaround for performance issue. See https://github.com/coiled/coiled-runtime/issues/79
-    # for more details. Replace with the line below once using `dask>=2022.04.2`.
-    # df_shuffled.to_parquet(shuffled_url, storage_options=s3_storage_options)
-    result = df_shuffled.to_parquet(
-        shuffled_url, compute=False, storage_options=s3_storage_options
-    )
-    dask.compute(result)
+    df_shuffled.to_parquet(shuffled_url, storage_options=s3_storage_options)
