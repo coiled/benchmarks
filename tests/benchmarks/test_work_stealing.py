630495a6d75350f1da0a2c3fde0bc8fa93b9f037
Hendrik Makait <hendrik@coiled.io>
2022-09-06 18:42:09 +0200

    Xfail trivial workload

--- tests/benchmarks/test_work_stealing.py
+++ tests/benchmarks/test_work_stealing.py
@@ -9,6 +9,7 @@ from dask import delayed, utils
 from distributed import Client, wait
 
 
+@pytest.mark.xfail(reason="https://github.com/dask/distributed/issues/6573")
 def test_trivial_workload_should_not_cause_work_stealing(small_client):
     root = delayed(lambda n: "x" * n)(utils.parse_bytes("1MiB"), dask_key_name="root")
     results = [delayed(lambda *args: None)(root, i) for i in range(10000)]
