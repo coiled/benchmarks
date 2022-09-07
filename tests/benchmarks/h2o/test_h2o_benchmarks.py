e5d48ee73c94a05af6c56ab942aa5e9266dd829b
James Bourbeau <jrbourbeau@gmail.com>
2022-09-07 10:30:12 -0500

    Test CI

--- tests/benchmarks/h2o/test_h2o_benchmarks.py
+++ tests/benchmarks/h2o/test_h2o_benchmarks.py
@@ -51,12 +51,10 @@ def ddf(request):
 
 
 def test_q1(ddf, small_client):
-    ddf = ddf[["id1", "v1"]]
     ddf.groupby("id1", dropna=False, observed=True).agg({"v1": "sum"}).compute()
 
 
 def test_q2(ddf, small_client):
-    ddf = ddf[["id1", "id2", "v1"]]
     (
         ddf.groupby(["id1", "id2"], dropna=False, observed=True)
         .agg({"v1": "sum"})
@@ -65,7 +63,6 @@ def test_q2(ddf, small_client):
 
 
 def test_q3(ddf, small_client):
-    ddf = ddf[["id3", "v1", "v3"]]
     (
         ddf.groupby("id3", dropna=False, observed=True)
         .agg({"v1": "sum", "v3": "mean"})
@@ -74,7 +71,6 @@ def test_q3(ddf, small_client):
 
 
 def test_q4(ddf, small_client):
-    ddf = ddf[["id4", "v1", "v2", "v3"]]
     (
         ddf.groupby("id4", dropna=False, observed=True)
         .agg({"v1": "mean", "v2": "mean", "v3": "mean"})
@@ -83,7 +79,6 @@ def test_q4(ddf, small_client):
 
 
 def test_q5(ddf, small_client):
-    ddf = ddf[["id6", "v1", "v2", "v3"]]
     (
         ddf.groupby("id6", dropna=False, observed=True)
         .agg({"v1": "sum", "v2": "sum", "v3": "sum"})
@@ -92,7 +87,6 @@ def test_q5(ddf, small_client):
 
 
 def test_q7(ddf, small_client):
-    ddf = ddf[["id3", "v1", "v2"]]
     (
         ddf.groupby("id3", dropna=False, observed=True)
         .agg({"v1": "max", "v2": "min"})
