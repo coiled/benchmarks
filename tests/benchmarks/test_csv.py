a960d21da6c44e82c47723271355d585309d4ca4
James Bourbeau <jrbourbeau@users.noreply.github.com>
2022-09-29 14:26:44 -0500

    Remove quickstart examples (#391)

---
+++ tests/benchmarks/test_csv.py
@@ -0,0 +1,20 @@
+import dask.dataframe as dd
+import pandas as pd
+
+
+def test_csv_basic(small_client):
+    ddf = dd.read_csv(
+        "s3://coiled-runtime-ci/nyc-tlc/yellow_tripdata_2019_csv/yellow_tripdata_2019-*.csv",
+        dtype={
+            "payment_type": "UInt8",
+            "VendorID": "UInt8",
+            "passenger_count": "UInt8",
+            "RatecodeID": "UInt8",
+        },
+        blocksize="16 MiB",
+    ).persist()
+
+    result = ddf.groupby("passenger_count").tip_amount.mean().compute()
+
+    assert isinstance(result, pd.Series)
+    assert not result.empty
--- tests/runtime/test_coiled.py
+++ tests/runtime/test_coiled.py
@@ -1,36 +1,4 @@
 import coiled
-import dask.dataframe as dd
-import pandas as pd
-
-
-def test_quickstart_csv(small_client):
-    ddf = dd.read_csv(
-        "s3://coiled-runtime-ci/nyc-tlc/yellow_tripdata_2019_csv/yellow_tripdata_2019-*.csv",
-        dtype={
-            "payment_type": "UInt8",
-            "VendorID": "UInt8",
-            "passenger_count": "UInt8",
-            "RatecodeID": "UInt8",
-        },
-        blocksize="16 MiB",
-    ).persist()
-
-    result = ddf.groupby("passenger_count").tip_amount.mean().compute()
-
-    assert isinstance(result, pd.Series)
-    assert not result.empty
-
-
-def test_quickstart_parquet(small_client):
-    ddf = dd.read_parquet(
-        "s3://coiled-runtime-ci/nyc-tlc/yellow_tripdata_2019_parquet/yellow_tripdata_2019-*.parquet",
-        columns=["passenger_count", "tip_amount"],
-    ).persist()
-
-    result = ddf.groupby("passenger_count").tip_amount.mean().compute()
-
-    assert isinstance(result, pd.Series)
-    assert not result.empty
 
 
 def test_reconnect(small_cluster):
