d476f7ffe1ed25756e72666f43791df2290700aa
ncclementi <natyclementi@gmail.com>
2022-09-29 14:15:35 -0500

    remove assert and try only 0.1

--- tests/benchmarks/test_join.py
+++ tests/benchmarks/test_join.py
@@ -4,7 +4,7 @@ import pytest
 from ..utils_test import cluster_memory, timeseries_of_size
 
 
-@pytest.mark.parametrize("mem_mult", [0.1, 1, 10])
+@pytest.mark.parametrize("mem_mult", [0.1])  # [0.1, 1, 10]
 def test_join_big(small_client, mem_mult):
     memory = cluster_memory(small_client)  # 76.66 GiB
 
@@ -21,11 +21,10 @@ def test_join_big(small_client, mem_mult):
     df2_big["x2"] = df2_big["x"] * 1e9
     df2_big = df2_big.astype({"x2": "int"})
 
-    dd_result = dd.merge(df1_big, df2_big, on="x2", how="inner").compute()
-    assert dd_result
+    dd.merge(df1_big, df2_big, on="x2", how="inner").compute()
 
 
-@pytest.mark.parametrize("mem_mult", [0.1, 1, 10])
+@pytest.mark.parametrize("mem_mult", [0.1])  # [0.1, 1, 10]
 def test_join_big_small(small_client, mem_mult):
     memory = cluster_memory(small_client)  # 76.66 GiB
 
@@ -43,5 +42,4 @@ def test_join_big_small(small_client, mem_mult):
     df_small["x2"] = df_small["x"] * 1e9
     df_small = df_small.astype({"x2": "int"})
 
-    dd_result = dd.merge(df_big, df_small, on="x2", how="inner").compute()
-    assert dd_result
+    dd.merge(df_big, df_small, on="x2", how="inner").compute()
