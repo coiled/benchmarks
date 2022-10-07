144c87b4ca7cda5fcfa89d48f79fae1fee9a3933
ncclementi <natyclementi@gmail.com>
2022-10-07 14:45:56 -0400

    Use details_url in test_cluster_creation

--- tests/runtime/test_cluster_creation.py
+++ tests/runtime/test_cluster_creation.py
@@ -12,17 +12,8 @@ def test_default_cluster_spinup_time(request, auto_benchmark_time, test_run_benc
     with Cluster(
         name=f"{request.node.originalname}-{uuid.uuid4().hex[:8]}", package_sync=True
     ) as cluster:
-        cluster_id = cluster.cluster_id
-        cluster_acc = cluster.account
-        details_url = (
-            f"https://cloud.coiled.io/{cluster_acc}/clusters/{cluster_id}/details"
-        )
-
         test_run_benchmark.cluster_name = cluster.name
-        test_run_benchmark.cluster_id = cluster_id
-        # Replace corresponding lines in next coiled release where
-        # details_url will be available as client.cluster.details_url
-        # test_run_benchmark.cluster_details_url = client.cluster.details_url
-        test_run_benchmark.cluster_details_url = details_url
+        test_run_benchmark.cluster_id = cluster.cluster_id
+        test_run_benchmark.cluster_details_url = cluster.details_url
 
         pass
