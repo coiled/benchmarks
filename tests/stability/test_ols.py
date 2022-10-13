db1b0f7f3d5183d1a50bf454f51f835adfc73228
Greg Hayes <greg.hayes@coiled.io>
2022-10-13 09:14:34 +0200

    Update tests/stability/test_ols.py
    Co-authored-by: James Bourbeau <jrbourbeau@users.noreply.github.com>

--- tests/stability/test_ols.py
+++ tests/stability/test_ols.py
@@ -3,7 +3,10 @@ import dask.array as da
 from ..utils_test import wait
 
 
-def test_ols(small_client, num_samples=int(1e8), num_coeffs=100, chunksize=int(1e6)):
+def test_ols(small_client):
+    num_samples = int(1e8)
+    num_coeffs = 100
+    chunksize = int(1e6)
     beta = da.random.normal(size=(num_coeffs,))
     X = da.random.normal(size=(num_samples, num_coeffs), chunks=(chunksize, -1))
     y = X @ beta + da.random.normal(size=(num_samples,), chunks=(chunksize,))
