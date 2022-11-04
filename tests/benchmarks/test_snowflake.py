be54472bb71b5b8aa64673e791585e8a9af47ff8
Paul Hobson <pmhobson@gmail.com>
2022-11-03 18:57:28 -0700

    more granular benchmarks

--- tests/benchmarks/test_snowflake.py
+++ tests/benchmarks/test_snowflake.py
@@ -4,6 +4,7 @@ import uuid
 import dask
 import pytest
 from dask_snowflake import read_snowflake, to_snowflake
+from distributed import Client
 from snowflake.sqlalchemy import URL
 from sqlalchemy import create_engine
 
@@ -35,18 +36,39 @@ def connection_kwargs():
 @pytest.mark.skipif(
     "SNOWFLAKE_USER" not in os.environ.keys(), reason="no snowflake credentials"
 )
-def test_write_to_snowflake(table, connection_kwargs, small_client):
+def test_write_to_snowflake(
+    table, connection_kwargs, small_cluster, upload_cluster_dump, benchmark_all
+):
+
     ddf = dask.datasets.timeseries(
         start="2000-01-01", end="2000-03-31", freq="1T", partition_freq="1W"
     )
-    to_snowflake(ddf, name=table, connection_kwargs=connection_kwargs)
 
-    if False:
-        query = f"SELECT * FROM {table}"
-        (
-            read_snowflake(query, connection_kwargs=connection_kwargs)
-            .rename(columns=str.upper)  # snowflake's column casing is weird
-            .sort_values(by="X")  # sort needed to re-sync the partitions
-            .reset_index(drop=True)
-            .compute()
-        )
+    with Client(small_cluster) as client:
+        small_cluster.scale(10)
+        client.wait_for_workers(10)
+        with upload_cluster_dump(client), benchmark_all(client):
+            to_snowflake(ddf, name=table, connection_kwargs=connection_kwargs)
+
+    with Client(small_cluster) as client:
+        small_cluster.scale(10)
+        client.wait_for_workers(10)
+
+        raw_query = f"SELECT * FROM {table}"
+
+        with upload_cluster_dump(client), benchmark_all(client):
+            reader = read_snowflake(raw_query, connection_kwargs=connection_kwargs)
+            reader.compute()
+
+        with upload_cluster_dump(client), benchmark_all(client):
+            reader = (
+                read_snowflake(raw_query, connection_kwargs=connection_kwargs)
+                .groupby("NAME")["X"]
+                .median()
+            )
+            reader.compute()
+
+        with upload_cluster_dump(client), benchmark_all(client):
+            median_query = f"SELECT NAME, MEDIAN(X) FROM {table} GROUP BY NAME"
+            reader = read_snowflake(median_query, connection_kwargs=connection_kwargs)
+            reader.compute()
