ceeb35e29d50f7b7a830fb4faf9660b3d35b3717
crusaderky <crusaderky@gmail.com>
2022-11-07 19:25:40 +0000

    rerun 10 times

--- tests/runtime/test_coiled.py
+++ tests/runtime/test_coiled.py
@@ -5,12 +5,14 @@ from distributed import Client
 def test_cluster_reconnect(small_cluster, get_cluster_info, benchmark_time):
     """How quickly can we reconnect to an existing cluster?"""
     with get_cluster_info(small_cluster), benchmark_time:
-        with Cluster(name=small_cluster.name, shutdown_on_close=False):
-            pass
+        for _ in range(10):
+            with Cluster(name=small_cluster.name, shutdown_on_close=False):
+                pass
 
 
 def test_client_connect(small_cluster, get_cluster_info, benchmark_time):
     """How quickly can we connect a client to an existing cluster?"""
     with get_cluster_info(small_cluster), benchmark_time:
-        with Client(small_cluster):
-            pass
+        for _ in range(10):
+            with Client(small_cluster):
+                pass
