ef40a8115201d05a8dce1c61bc8bf653c1309e07
crusaderky <crusaderky@gmail.com>
2022-11-07 17:38:31 +0000

    Measure client and cluster connect time

--- tests/runtime/test_cluster_creation.py
+++ tests/runtime/test_cluster_creation.py
@@ -4,20 +4,18 @@ from coiled import Cluster
 
 
 def test_default_cluster_spinup_time(
-    auto_benchmark_time, test_run_benchmark, gitlab_cluster_tags
+    benchmark_time, gitlab_cluster_tags, get_cluster_info
 ):
     """Note: this test must be kept in a separate module from the tests that use the
     small_cluster fixture (which has the scope=module) or its child small_client.
     This prevents having the small_cluster sitting idle for 5+ minutes while this test
     is running.
     """
-    with Cluster(
-        name=f"test_default_cluster_spinup_time-{uuid.uuid4().hex[:8]}",
-        package_sync=True,
-        tags=gitlab_cluster_tags,
-    ) as cluster:
-        test_run_benchmark.cluster_name = cluster.name
-        test_run_benchmark.cluster_id = cluster.cluster_id
-        test_run_benchmark.cluster_details_url = cluster.details_url
-
-        pass
+    with benchmark_time:
+        with Cluster(
+            name=f"test_default_cluster_spinup_time-{uuid.uuid4().hex[:8]}",
+            package_sync=True,
+            tags=gitlab_cluster_tags,
+        ) as cluster:
+            with get_cluster_info(cluster):
+                pass
--- tests/runtime/test_coiled.py
+++ tests/runtime/test_coiled.py
@@ -1,7 +1,16 @@
-import coiled
+from coiled import Cluster
+from distributed import Client
 
 
-def test_reconnect(small_cluster):
+def test_cluster_reconnect(small_cluster, get_cluster_info, benchmark_time):
     """How quickly can we reconnect to an existing cluster?"""
-    with coiled.Cluster(name=small_cluster.name):
-        pass
+    with get_cluster_info(small_cluster), benchmark_time:
+        with Cluster(name=small_cluster.name, shutdown_on_close=False):
+            pass
+
+
+def test_client_connect(small_cluster, get_cluster_info, benchmark_time):
+    """How quickly can we connect a client to an existing cluster?"""
+    with get_cluster_info(small_cluster), benchmark_time:
+        with Client(small_cluster):
+            pass
