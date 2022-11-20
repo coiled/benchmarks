c8f2b6157f4cc426eac5d6aa5a37d5aadeca497a
Nat Tabris <nat@coiled.io>
2022-11-20 09:47:00 -0700

    4xlarge spill

--- tests/stability/test_spill.py
+++ tests/stability/test_spill.py
@@ -6,14 +6,17 @@ from coiled import Cluster
 from dask.distributed import Client, wait
 from toolz import merge
 
+N_SPILL_WORKERS = 2
+GB_MEM_PER_SPILL_WORKER = 64
+
 
 @pytest.fixture(scope="module")
 def spill_cluster(dask_env_variables, gitlab_cluster_tags):
     with Cluster(
         f"spill-{uuid.uuid4().hex[:8]}",
-        n_workers=5,
+        n_workers=N_SPILL_WORKERS,
         package_sync=True,
-        worker_vm_types=["m6id.xlarge"],
+        worker_vm_types=["m6id.4xlarge"],
         scheduler_vm_types=["m6i.xlarge"],
         wait_for_workers=True,
         backend_options={
@@ -43,8 +46,8 @@ def spill_cluster(dask_env_variables, gitlab_cluster_tags):
 @pytest.fixture
 def spill_client(spill_cluster, upload_cluster_dump, benchmark_all):
     with Client(spill_cluster) as client:
-        spill_cluster.scale(5)
-        client.wait_for_workers(5)
+        spill_cluster.scale(N_SPILL_WORKERS)
+        client.wait_for_workers(N_SPILL_WORKERS)
         client.restart()
         with upload_cluster_dump(client), benchmark_all(client):
             yield client
@@ -53,7 +56,9 @@ def spill_client(spill_cluster, upload_cluster_dump, benchmark_all):
 @pytest.mark.stability
 @pytest.mark.parametrize("keep_around", (True, False))
 def test_spilling(spill_client, keep_around):
-    arr = da.random.random((128, 2**27)).persist()  # 128 GiB, ~2x memory
+    arr = da.random.random(
+        (GB_MEM_PER_SPILL_WORKER * N_SPILL_WORKERS * 2, 2**27)
+    ).persist()  # ~2x memory
     wait(arr)
     fut = spill_client.compute(arr.sum())
     if not keep_around:
