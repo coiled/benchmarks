9ffbc2bcab012cac96134b2103e7d294fb02ae80
Greg Hayes <greg.hayes@coiled.io>
2022-11-29 09:36:31 -0600

    OLS Workload (#443)
    * Linting
    
    * Update tests/stability/test_ols.py
    
    Co-authored-by: James Bourbeau <jrbourbeau@users.noreply.github.com>
    
    * Show tests
    
    * Moved test_ols to test_array.py
    
    * Clean up test_ols
    
    * Test array file
    
    * Fix test_dataframe.py
    
    * Resizing X relative to size of cluster
    
    * Sets target_nbytes to 0.50
    
    Co-authored-by: James Bourbeau <jrbourbeau@users.noreply.github.com>

--- tests/stability/test_array.py
+++ tests/stability/test_array.py
@@ -1,6 +1,8 @@
 import dask.array as da
 import pytest
 
+from ..utils_test import cluster_memory, scaled_array_shape, wait
+
 
 @pytest.mark.stability
 def test_rechunk_in_memory(small_client):
@@ -12,3 +14,18 @@ def test_rechunk_in_memory(small_client):
 def test_rechunk_out_of_memory(small_client):
     x = da.random.random((100000, 100000))
     x.rechunk((50000, 20)).rechunk((20, 50000)).sum().compute()
+
+
+@pytest.mark.stability
+def test_ols(small_client):
+    chunksize = int(1e6)
+    memory = cluster_memory(small_client)
+    target_nbytes = memory * 0.50
+    target_shape = scaled_array_shape(target_nbytes, ("x", 100))
+    num_samples, num_coeffs = target_shape[0], target_shape[-1]
+    beta = da.random.normal(size=(num_coeffs,))
+    X = da.random.normal(size=(num_samples, num_coeffs), chunks=(chunksize, -1))
+    y = X @ beta + da.random.normal(size=(num_samples,), chunks=(chunksize,))
+    beta_hat = da.linalg.solve(X.T @ X, X.T @ y)  # normal eq'n
+    y_hat = X @ beta_hat
+    wait(y_hat, small_client, 20 * 60)
