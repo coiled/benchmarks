ac15b497834b0a13896ad2a569f82de9ba8f75da
crusaderky <crusaderky@gmail.com>
2022-11-30 15:14:28 +0000

    remove test_client_connect

--- tests/runtime/test_coiled.py
+++ tests/runtime/test_coiled.py
@@ -1,18 +1,8 @@
 from coiled import Cluster
-from distributed import Client
 
 
 def test_cluster_reconnect(small_cluster, get_cluster_info, benchmark_time):
     """How quickly can we reconnect to an existing cluster?"""
     with get_cluster_info(small_cluster), benchmark_time:
-        for _ in range(10):
-            with Cluster(name=small_cluster.name, shutdown_on_close=False):
-                pass
-
-
-def test_client_connect(small_cluster, get_cluster_info, benchmark_time):
-    """How quickly can we connect a client to an existing cluster?"""
-    with get_cluster_info(small_cluster), benchmark_time:
-        for _ in range(10):
-            with Client(small_cluster):
-                pass
+        with Cluster(name=small_cluster.name, shutdown_on_close=False):
+            pass
