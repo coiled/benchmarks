02207c9b95a0d639a4e49e93ab87948fecbe2d23
Hendrik Makait <hendrik@coiled.io>
2022-11-30 16:36:21 +0100

    Parametrize shuffle type for shuffle tests

--- tests/stability/test_shuffle.py
+++ tests/stability/test_shuffle.py
@@ -3,20 +3,25 @@ import dask.dataframe as dd
 import pytest
 
 
+@pytest.fixture(params=["tasks", "p2p"])
+def shuffle(request):
+    return request.param
+
+
 @pytest.mark.stability
-def test_shuffle_simple(small_client):
+def test_shuffle_simple(small_client, shuffle):
     df = dask.datasets.timeseries(
         start="2000-01-01", end="2000-12-31", freq="1s", partition_freq="1D"
     )
 
-    sdf = df.shuffle(on="x")
+    sdf = df.shuffle(on="x", shuffle=shuffle)
 
     sdf_size = sdf.memory_usage(index=True).sum() / (1024.0**3)
     assert sdf_size.compute() > 1.0  # 1.0 GB
 
 
 @pytest.mark.stability
-def test_shuffle_parquet(small_client, s3_url, s3_storage_options):
+def test_shuffle_parquet(small_client, s3_url, s3_storage_options, shuffle):
     # Write synthetic dataset to S3
     # Notes on how `freq` impacts total dataset size:
     #   - 100ms ~12GB
@@ -31,7 +36,7 @@ def test_shuffle_parquet(small_client, s3_url, s3_storage_options):
     # Test `read_parquet` + `shuffle` + `to_parquet` works
     shuffled_url = s3_url + "/shuffled.parquet"
     df_shuffled = dd.read_parquet(dataset_url, storage_options=s3_storage_options)
-    df_shuffled = df_shuffled.shuffle(on="x")
+    df_shuffled = df_shuffled.shuffle(on="x", shuffle=shuffle)
     # Workaround for performance issue. See https://github.com/coiled/coiled-runtime/issues/79
     # for more details. Replace with the line below once using `dask>=2022.04.2`.
     # df_shuffled.to_parquet(shuffled_url, storage_options=s3_storage_options)
