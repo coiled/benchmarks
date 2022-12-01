4e9e300e201aeb2aacd70e5185ff226ca474946c
ncclementi <natyclementi@gmail.com>
2022-12-01 17:32:50 -0500

    update setup.py for release 0.2.0

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ python_requires, install_requires = get_requirements()
 
 setup(
     name="coiled-runtime",
-    version="0.1.1",
+    version="0.2.0",
     description="Simple and fast way to get started with Dask",
     url="https://github.com/coiled/coiled-runtime",
     license="BSD",
