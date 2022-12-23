61b9001497ba97b543151488c636779a44132c00
Hendrik Makait <hendrik@coiled.io>
2022-12-23 20:06:05 +0100

    Fix params

--- tests/benchmarks/test_join.py
+++ tests/benchmarks/test_join.py
@@ -9,12 +9,17 @@ params = [
     (0.1, "tasks"),
     # shuffling takes a long time with 1 or higher
     (0.1, "p2p"),
-    pytest.param(
-        (1, "p2p"), marks=pytest.mark.skip(reason="client OOMs, see coiled-runtime#633")
+    (
+        1,
+        pytest.param(
+            "p2p", marks=pytest.mark.skip(reason="client OOMs, see coiled-runtime#633")
+        ),
     ),
-    pytest.param(
-        (10, "p2p"),
-        marks=pytest.mark.skip(reason="client OOMs, see coiled-runtime#633"),
+    (
+        10,
+        pytest.param(
+            "p2p", marks=pytest.mark.skip(reason="client OOMs, see coiled-runtime#633")
+        ),
     ),
 ]
 
