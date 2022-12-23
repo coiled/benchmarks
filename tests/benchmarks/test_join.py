b9880e79ae2856f4a593f6564c3a3fcf363ba664
Hendrik Makait <hendrik@coiled.io>
2022-12-23 20:15:06 +0100

    Minor

--- tests/benchmarks/test_join.py
+++ tests/benchmarks/test_join.py
@@ -9,17 +9,11 @@ params = [
     (0.1, "tasks"),
     # shuffling takes a long time with 1 or higher
     (0.1, "p2p"),
-    (
-        pytest.param(
-            1, marks=pytest.mark.skip(reason="client OOMs, see coiled-runtime#633")
-        ),
-        "p2p",
+    pytest.param(
+        1, "p2p", marks=pytest.mark.skip(reason="client OOMs, see coiled-runtime#633")
     ),
-    (
-        pytest.param(
-            10, marks=pytest.mark.skip(reason="client OOMs, see coiled-runtime#633")
-        ),
-        "p2p",
+    pytest.param(
+        10, "p2p", marks=pytest.mark.skip(reason="client OOMs, see coiled-runtime#633")
     ),
 ]
 
