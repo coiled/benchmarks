0dd6bec28c22b56d4d05197163526e39a29f0766
Hendrik Makait <hendrik@coiled.io>
2023-01-10 15:03:23 +0100

    Serialize migrations

--- alembic/versions/a97d9375430f_default_parameter_for_test_dataframe_py_.py
+++ alembic/versions/a97d9375430f_default_parameter_for_test_dataframe_py_.py
@@ -1,7 +1,7 @@
 """Default parameter for test_dataframe.py::test_shuffle
 
 Revision ID: a97d9375430f
-Revises: a9363331e323
+Revises: 967e298408ed
 Create Date: 2023-01-03 19:36:30.469391
 
 """
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'a97d9375430f'
-down_revision = 'a9363331e323'
+down_revision = '967e298408ed'
 branch_labels = None
 depends_on = None
 
