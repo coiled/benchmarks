92be440d41723544d53003165b64818c36d6b080
Hendrik Makait <hendrik@coiled.io>
2023-01-20 17:10:37 +0100

    Adjust join tests

--- tests/benchmarks/test_join.py
+++ tests/benchmarks/test_join.py
@@ -8,6 +8,7 @@ from ..utils_test import cluster_memory, run_up_to_nthreads, timeseries_of_size,
 params = [
     (0.1, "tasks"),
     (0.1, "p2p"),
+    (1, "tasks"),
     (1, "p2p"),
     # shuffling takes a long time with 10
     (10, "p2p"),
@@ -20,17 +21,21 @@ def test_join_big(small_client, mem_mult, shuffle):
     with dask.config.set(shuffle=shuffle):
         memory = cluster_memory(small_client)  # 76.66 GiB
 
-        df1_big = timeseries_of_size(memory * mem_mult)
-        df1_big["x2"] = df1_big["x"] * 1e9
-        df1_big = df1_big.astype({"x2": "int"})
+        df1_big = timeseries_of_size(
+            memory * mem_mult, dtypes={str(i): float for i in range(100)}
+        )  # 66.58 MiB partitions
+        df1_big["predicate"] = df1_big["0"] * 1e9
+        df1_big = df1_big.astype({"predicate": "int"})
 
-        df2_big = timeseries_of_size(memory * mem_mult)
+        df2_big = timeseries_of_size(
+            memory * mem_mult, dtypes={str(i): float for i in range(100)}
+        )  # 66.58 MiB partitions
 
         # Control cardinality on column to join - this produces cardinality ~ to len(df)
-        df2_big["x2"] = df2_big["x"] * 1e9
-        df2_big = df2_big.astype({"x2": "int"})
+        df2_big["predicate"] = df2_big["0"] * 1e9
+        df2_big = df2_big.astype({"predicate": "int"})
 
-        join = dd.merge(df1_big, df2_big, on="x2", how="inner")
+        join = dd.merge(df1_big, df2_big, on="predicate", how="inner")
         result = join.size
         wait(result, small_client, 20 * 60)
 
@@ -40,17 +45,21 @@ def test_join_big_small(small_client, mem_mult, shuffle):
     with dask.config.set(shuffle=shuffle):
         memory = cluster_memory(small_client)  # 76.66 GiB
 
-        df_big = timeseries_of_size(memory * mem_mult)
+        df_big = timeseries_of_size(
+            memory * mem_mult, dtypes={str(i): float for i in range(100)}
+        )  # 66.58 MiB partitions
 
         # Control cardinality on column to join - this produces cardinality ~ to len(df)
-        df_big["x2"] = df_big["x"] * 1e9
+        df_big["predicate"] = df_big["0"] * 1e9
         df_big = df_big.astype({"x2": "int"})
 
-        df_small = timeseries_of_size("50 MB")  # make it obviously small
+        df_small = timeseries_of_size(
+            "100 MB", dtypes={str(i): float for i in range(100)}
+        )  # make it obviously small
 
-        df_small["x2"] = df_small["x"] * 1e9
-        df_small_pd = df_small.astype({"x2": "int"}).compute()
+        df_small["predicate"] = df_small["0"] * 1e9
+        df_small_pd = df_small.astype({"predicate": "int"}).compute()
 
-        join = dd.merge(df_big, df_small_pd, on="x2", how="inner")
+        join = dd.merge(df_big, df_small_pd, on="predicate", how="inner")
         result = join.size
         wait(result, small_client, 20 * 60)
