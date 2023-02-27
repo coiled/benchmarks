bcb2ca99c8b5b508a099820eea2e351af021ae8a
Hendrik Makait <hendrik@coiled.io>
2023-02-27 16:15:59 +0100

    Fix test join

--- tests/benchmarks/test_join.py
+++ tests/benchmarks/test_join.py
@@ -3,28 +3,24 @@ import pytest
 
 from ..utils_test import cluster_memory, run_up_to_nthreads, timeseries_of_size, wait
 
-# (mem_mult, shuffle)
-params = [
-    (0.1, "tasks"),
-    (0.1, "p2p"),
-    (1, "tasks"),
-    (1, "p2p"),
-]
+
+@pytest.fixture(params=[0.1, 1])
+def memory_multiplier(request):
+    return request.param
 
 
 @run_up_to_nthreads("small_cluster", 40, reason="Does not finish")
-@pytest.mark.parametrize("mem_mult, shuffle", params)
-def test_join_big(small_client, mem_mult, configure_shuffling):
+def test_join_big(small_client, memory_multiplier, configure_shuffling):
     memory = cluster_memory(small_client)  # 76.66 GiB
 
     df1_big = timeseries_of_size(
-        memory * mem_mult, dtypes={str(i): float for i in range(100)}
+        memory * memory_multiplier, dtypes={str(i): float for i in range(100)}
     )  # 66.58 MiB partitions
     df1_big["predicate"] = df1_big["0"] * 1e9
     df1_big = df1_big.astype({"predicate": "int"})
 
     df2_big = timeseries_of_size(
-        memory * mem_mult, dtypes={str(i): float for i in range(100)}
+        memory * memory_multiplier, dtypes={str(i): float for i in range(100)}
     )  # 66.58 MiB partitions
 
     # Control cardinality on column to join - this produces cardinality ~ to len(df)
@@ -36,12 +32,11 @@ def test_join_big(small_client, mem_mult, configure_shuffling):
     wait(result, small_client, 20 * 60)
 
 
-@pytest.mark.parametrize("mem_mult, shuffle", params)
-def test_join_big_small(small_client, mem_mult, configure_shuffling):
+def test_join_big_small(small_client, memory_multiplier, configure_shuffling):
     memory = cluster_memory(small_client)  # 76.66 GiB
 
     df_big = timeseries_of_size(
-        memory * mem_mult, dtypes={str(i): float for i in range(100)}
+        memory * memory_multiplier, dtypes={str(i): float for i in range(100)}
     )  # 66.58 MiB partitions
 
     # Control cardinality on column to join - this produces cardinality ~ to len(df)
