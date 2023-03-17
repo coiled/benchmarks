7e1452f2e4a56cc44e9a5ce913257ddee8585049
Irina Truong <i.chernyavska@gmail.com>
2023-03-17 15:59:36 -0700

    Can't use convert_string with pandas 1.5.

--- tests/benchmarks/test_arrow.py
+++ tests/benchmarks/test_arrow.py
@@ -1,42 +1,52 @@
+import pytest
+import pandas as pd
+
 from ..utils_test import cluster_memory, timeseries_of_size, wait
 
 
-def test_unique(small_client, convert_string):
+@pytest.mark.skipif()
+def test_unique(small_client):
     """Find unique values"""
     memory = cluster_memory(small_client)
     df = timeseries_of_size(memory)
-    result = df.name.unique()
+    s = df.name.astype(pd.StringDtype("pyarrow")).persist()
+    result = s.unique()
     wait(result, small_client, 10 * 60)
 
 
-def test_contains(small_client, convert_string):
+def test_contains(small_client):
     """String contains"""
     memory = cluster_memory(small_client)
     df = timeseries_of_size(memory)
-    result = df.name.str.contains("a")
+    s = df.name.astype(pd.StringDtype("pyarrow")).persist()
+    result = s.str.contains("a")
     wait(result, small_client, 10 * 60)
 
 
-def test_startswith(small_client, convert_string):
+def test_startswith(small_client):
     """String starts with"""
     memory = cluster_memory(small_client)
     df = timeseries_of_size(memory)
-    result = df.name.str.startswith("B")
+    s = df.name.astype(pd.StringDtype("pyarrow")).persist()
+    result = s.str.startswith("B")
     wait(result, small_client, 10 * 60)
 
 
-def test_filter(small_client, convert_string):
+def test_filter(small_client):
     """How fast can we filter a DataFrame?"""
     memory = cluster_memory(small_client)
     df = timeseries_of_size(memory)
+    df.name = df.name.astype(pd.StringDtype("pyarrow"))
+    df = df.persist()
     name = df.head(1).name.iloc[0]  # Get first name that appears
     result = df[df.name == name]
     wait(result, small_client, 10 * 60)
 
 
-def test_value_counts(small_client, convert_string):
+def test_value_counts(small_client):
     """Value counts on string values"""
     memory = cluster_memory(small_client)
     df = timeseries_of_size(memory)
-    result = df.name.value_counts()
+    s = df.name.astype(pd.StringDtype("pyarrow")).persist()
+    result = s.value_counts()
     wait(result, small_client, 10 * 60)
