d003b161aae921409dedcc3b7fa6e24bbb5c4613
James Bourbeau <jrbourbeau@gmail.com>
2023-03-31 14:41:20 -0500

    Add full shuffle example

--- tests/workflows/test_rechunk.py
+++ tests/workflows/test_rechunk.py
@@ -47,3 +47,14 @@ def test_rechunk(rechunk_client, s3_url):
     assert x.chunksize == (6443, 100, 100)  # 61.45 MiB (dtype = int8)
     y = x.rechunk((6443, 100, 200))  # 122.89 MiB
     y.to_zarr(s3_url)
+
+
+def test_rechunk_full_shuffle(rechunk_client, s3_url):
+    # Test a "full shuffle" rechunk where every input chunk goes into every output chunk
+    # Dataset is 196.72 GiB
+    x = da.from_zarr(
+        "s3://yuvipanda-test1/cmr/gpm3imergdl.zarr/HQprecipitation",
+        storage_options={"anon": True},
+    )
+    y = x.rechunk((-1, 36, 180))
+    y.to_zarr(s3_url)
