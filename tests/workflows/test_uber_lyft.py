f09ad6af53f046c33de221d716ad86274337ca0c
Irina Truong <i.chernyavska@gmail.com>
2023-04-06 13:02:16 -0700

    Add more aggs, similar to tutorial.

--- tests/workflows/test_uber_lyft.py
+++ tests/workflows/test_uber_lyft.py
@@ -65,21 +65,56 @@ def test_explore_dataset(uber_lyft_client):
     # add a column to indicate company, instead of license number
     ddf["company"] = ddf.hvfhs_license_num.replace(taxi_companies)
 
+    # add a column to indicate that tip was given
+    ddf["tip_flag"] = ddf.tips > 0
+
+    # what percentage of fare is the tip
+    ddf["tip_percentage"] = ddf.tips / ddf.base_passenger_fare
+
     # persist so we only read once
     ddf = ddf.persist()
 
     # how many riders tip, in general
     (ddf.tips != 0).mean().compute()
 
-    # how many riders tip, grouped by company
-    def over_zero(x):
-        return x[x > 0].count() / x.count()
-
-    ddf.groupby("company").tips.apply(over_zero, meta=("tips", float)).compute()
+    ddf.groupby("company").tip_flag.value_counts().compute()
 
     # what's the largest and average pay and tips that driver got, by company
     ddf.groupby("company").agg(
         {"driver_pay": ["max", "mean"], "tips": ["max", "mean"]}
+    ).compute()
+
+    # tip percentage mean of trip with tip
+    ddf.loc[lambda x: x.tip_flag].groupby("company").tip_percentage.mean().compute()
+
+    # sample the dataset to filter outliers, result is a pandas dataframe
+    df_tiny = (
+        ddf.loc[lambda x: x.tip_flag][["trip_miles", "base_passenger_fare", "tips"]]
+        .sample(frac=0.001)
+        .compute()
+    )
+
+    # filter outliers by tips values
+    tips_filter_vals = df_tiny.tips.quantile([0.25, 0.75]).values
+    tips_condition = ddf.tips.between(*tips_filter_vals)
+
+    # filter outliers by fare values
+    fare_filter_vals = df_tiny.base_passenger_fare.quantile([0.25, 0.75]).values
+    fares_condition = ddf.base_passenger_fare.between(*fare_filter_vals)
+
+    # filter outliers by miles values
+    miles_filter_vals = df_tiny.trip_miles.quantile([0.25, 0.75]).values
+    miles_condition = ddf.trip_miles.between(*miles_filter_vals)
+
+    # filter the dataset to remove outliers, and persist again
+    ddf = ddf.loc[(tips_condition & fares_condition) & miles_condition].persist()
+
+    # now compute tip stats again
+    ddf.groupby("company").tip_percentage.mean().compute()
+
+    # average trip time by provider
+    ddf.groupby("hvfhs_license_num").trip_time.agg(
+        ["min", "max", "mean", "std"]
     ).compute()
 
     # ride count per company
