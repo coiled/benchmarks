64780cd1da631755b73a5cb2c024a81bd6729ecd
crusaderky <crusaderky@gmail.com>
2023-04-06 12:04:43 +0100

    Fix failure in 'pip installation' CI every time there's a new release (#768)

--- tests/runtime/test_build.py
+++ tests/runtime/test_build.py
@@ -103,6 +103,8 @@ def test_install_dist():
 @pytest.mark.latest_runtime
 def test_latest_coiled():
     # Ensure `coiled-runtime` installs the latest version of `coiled` by default
+    # This is installed from either conda-forge or pip, depending on which github action
+    # is running this test
     v_installed = Version(coiled.__version__)
 
     # Get latest `coiled` release version from conda-forge
@@ -111,8 +113,14 @@ def test_latest_coiled():
     )
     result = json.loads(output)
     v_latest = Version(result["coiled"][-1]["version"])
-
-    assert v_installed == v_latest
+    # conda can lag behind a few days from pip; allow for the next version too
+    v_allowed = {
+        v_latest,
+        Version(f"{v_latest.major}.{v_latest.minor}.{v_latest.micro + 1}"),
+        Version(f"{v_latest.major}.{v_latest.minor + 1}.0"),
+        Version(f"{v_latest.major + 1}.0.0"),
+    }
+    assert v_installed in v_allowed
 
 
 def test_version_warning_packages():
