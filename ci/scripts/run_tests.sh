8123acf237e4ff019a64d184735d3c6e2a58e913
James Bourbeau <jrbourbeau@gmail.com>
2023-04-13 16:02:29 -0500

    More

--- ci/scripts/run_tests.sh
+++ ci/scripts/run_tests.sh
@@ -4,13 +4,7 @@ set -o xtrace
 
 BENCHMARK="${BENCHMARK:-false}"
 
-# Ensure we run additional tests when testing the latest coiled-runtime
-if [[ $COILED_RUNTIME_VERSION = 'latest' || $COILED_RUNTIME_VERSION = 'upstream' ]]
-then
-  EXTRA_OPTIONS="--run-latest"
-else
-  EXTRA_OPTIONS=""
-fi
+EXTRA_OPTIONS="--run-latest"
 if [[ $BENCHMARK = 'true' ]]
 then
   EXTRA_OPTIONS="$EXTRA_OPTIONS --benchmark"
