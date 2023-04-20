e1568782e46340feffab003b5e08a516b1853d15
James Bourbeau <jrbourbeau@gmail.com>
2023-04-20 17:35:32 -0500

    Updates

--- tests/workflows/test_pytorch_optuna.py
+++ tests/workflows/test_pytorch_optuna.py
@@ -13,43 +13,8 @@ import torch.optim as optim
 import torch.utils.data
 from dask.distributed import Client, PipInstall, wait
 
-# Root directory for dataset
-dataroot = "data/celeba"
-
-# Number of Optuna trials
-n_trials = 500
-
-# Number of workers for dataloader
-# IMPORTANT w/ optuna; it launches a daemonic process, so PyTorch can't itself use it then.
-workers = 0
-
-# Batch size during training
-batch_size = 64
-
-# Spatial size of training images. All images will be resized to this
-#   size using a transformer.
-image_size = 64
-
-# Number of channels in the training images. For color images this is 3
-nc = 3
-
-# Size of z latent vector (i.e. size of generator input)
-nz = 100
-
-# Size of feature maps in generator
-ngf = 64
-
-# Size of feature maps in discriminator
-ndf = 64
-
-# Number of training epochs
-num_epochs = 5
-
-# Beta1 hyperparameter for Adam optimizers
-beta1 = 0.5
-
-# Number of GPUs available. Use 0 for CPU mode.
-ngpu = 1
+NC = 3  # Number of channels in the training images. For color images this is 3
+NZ = 100  # Size of z latent vector (i.e. size of generator input)
 
 
 @pytest.fixture(scope="module")
@@ -58,10 +23,8 @@ def pytorch_optuna_cluster(
     cluster_kwargs,
     github_cluster_tags,
 ):
-    name = f"pytorch-optuna-{uuid.uuid4().hex[:8]}"
-
     with coiled.Cluster(
-        name=name,
+        name=f"pytorch-optuna-{uuid.uuid4().hex[:8]}",
         environ=dask_env_variables,
         tags=github_cluster_tags,
         **cluster_kwargs["pytorch_optuna_cluster"],
@@ -81,161 +44,21 @@ def pytorch_optuna_client(
         pytorch_optuna_cluster.scale(n_workers)
         client.wait_for_workers(n_workers)
         client.upload_file(__file__)
+        # sync_packages won't work b/c local env won't have Cuda/GPU stuff
+        # and using only software environments won't pick up local project.
+        # Therefore, we'll install the stuff we need at runtime.
+        plugin = PipInstall(
+            [
+                "torch==2.0.0",
+                "git+https://github.com/optuna/optuna.git@378508ab6bddbad182bdfa0e8b3ad4bbb7040f00",
+            ],
+            pip_options=["--force-reinstall"],
+            restart=True,
+        )
+        client.register_worker_plugin(plugin)
         client.restart()
         with upload_cluster_dump(client), benchmark_all(client):
             yield client
-
-
-def test_pytorch_optuna_hyper_parameter_optimization(pytorch_optuna_client):
-    """Run example of running PyTorch and Optuna together on Dask"""
-
-    def objective(trial):
-        dataset = FakeImageDataset()
-
-        # Create the dataloader
-        dataloader = torch.utils.data.DataLoader(
-            dataset, batch_size=batch_size, shuffle=True, num_workers=workers
-        )
-
-        # Decide which device we want to run on
-        device = torch.device(
-            "cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu"
-        )
-
-        ########################
-        # Create the generator #
-        netG = Generator.from_trial(trial).to(device)
-
-        # Handle multi-GPU if desired
-        if (device.type == "cuda") and (ngpu > 1):
-            netG = nn.DataParallel(netG, list(range(ngpu)))
-
-        # Apply the ``weights_init`` function to randomly initialize all weights
-        #  to ``mean=0``, ``stdev=0.02``.
-        netG.apply(weights_init)
-
-        ############################
-        # Create the Discriminator #
-        netD = Discriminator.from_trial(trial).to(device)
-
-        # Handle multi-GPU if desired
-        if (device.type == "cuda") and (ngpu > 1):
-            netD = nn.DataParallel(netD, list(range(ngpu)))
-
-        # Apply the ``weights_init`` function to randomly initialize all weights
-        # like this: ``to mean=0, stdev=0.2``.
-        netD.apply(weights_init)
-
-        ########################################
-        # Remaining crierion, optimizers, etc. #
-        # Initialize the ``BCELoss`` function
-        criterion = nn.BCELoss()
-
-        # Establish convention for real and fake labels during training
-        real_label = 1.0
-        fake_label = 0.0
-
-        # Learning rate for optimizers
-        lr = 0.00001
-
-        # Setup Adam optimizers for both G and D
-        optimizerD = optim.Adam(netD.parameters(), lr=lr, betas=(beta1, 0.999))
-        optimizerG = optim.Adam(netG.parameters(), lr=lr, betas=(beta1, 0.999))
-
-        #################
-        # Training Loop #
-
-        # Lists to keep track of progress
-        G_losses = []
-        D_losses = []
-        iters = 0
-
-        print("Starting Training Loop...")
-        # For each epoch
-        for epoch in range(num_epochs):
-            # For each batch in the dataloader
-            for i, data in enumerate(dataloader, 0):
-                ############################
-                # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
-                ###########################
-                # Train with all-real batch
-                netD.zero_grad()
-                # Format batch
-                real_cpu = data[0].to(device)
-                b_size = real_cpu.size(0)
-                label = torch.full(
-                    (b_size,), real_label, dtype=torch.float, device=device
-                )
-                # Forward pass real batch through D
-                output = netD(real_cpu).view(-1)
-                # Calculate loss on all-real batch
-                errD_real = criterion(output, label)
-                # Calculate gradients for D in backward pass
-                errD_real.backward()
-
-                # Train with all-fake batch
-                # Generate batch of latent vectors
-                noise = torch.randn(b_size, nz, 1, 1, device=device)
-                # Generate fake image batch with G
-                fake = netG(noise)
-                label.fill_(fake_label)
-
-                # Classify all fake batch with D
-                output = netD(fake.detach()).view(-1)
-                # Calculate D's loss on the all-fake batch
-                errD_fake = criterion(output, label)
-                # Calculate the gradients for this batch, accumulated (summed) with previous gradients
-                errD_fake.backward()
-
-                # Compute error of D as sum over the fake and the real batches
-                errD = errD_real + errD_fake
-                # Update D
-                optimizerD.step()
-
-                ############################
-                # (2) Update G network: maximize log(D(G(z)))
-                ###########################
-                netG.zero_grad()
-                # fake labels are real for generator cost
-                label.fill_(real_label)
-                # Since we just updated D, perform another forward pass of all-fake batch through D
-                output = netD(fake).view(-1)
-                # Calculate G's loss based on this output
-                errG = criterion(output, label)
-                # Calculate gradients for G
-                errG.backward()
-
-                # Update G
-                optimizerG.step()
-
-                # Save Losses for plotting later
-                G_losses.append(errG.item())
-                D_losses.append(errD.item())
-
-                iters += 1
-
-            # Report to Optuna
-            trial.report(errD.item(), epoch)
-            if trial.should_prune():
-                raise optuna.exceptions.TrialPruned()
-        return errD.item()
-
-    # sync_packages won't work b/c local env won't have Cuda/GPU stuff
-    # and using only software environments won't pick up local project.
-    # Therefore, we'll install the stuff we need at runtime.
-    plugin = PipInstall(
-        ["torch==2.0.0", "optuna==3.1.1"], pip_options=["--force-reinstall"]
-    )
-    pytorch_optuna_client.register_worker_plugin(plugin)
-    pytorch_optuna_client.restart()
-
-    study = optuna.create_study(direction="minimize")
-    jobs = [
-        pytorch_optuna_client.submit(study.optimize, objective, n_trials=1, pure=False)
-        for _ in range(n_trials)
-    ]
-    _ = wait(jobs)
-    _ = [j.result() for j in jobs]
 
 
 # We create a fake image dataset. This ought to be replaced by
@@ -251,7 +74,9 @@ class FakeImageDataset(torch.utils.data.Dataset):
     def __getitem__(self, idx):
         label = self.labels[idx]
         torch.random.seed = label
-        img = torch.rand(3, image_size, image_size)
+        # Spatial size of training images. All images will be resized to this
+        # size using a transformer.
+        img = torch.rand(3, 64, 64)
         return img, label
 
 
@@ -264,85 +89,157 @@ def weights_init(m):
         nn.init.constant_(m.bias.data, 0)
 
 
-class Generator(nn.Module):
-    def __init__(self, ngpu, activation=None):
-        super(Generator, self).__init__()
-        self.ngpu = ngpu
-        self.main = nn.Sequential(
-            # input is Z, going into a convolution
-            nn.ConvTranspose2d(nz, ngf * 8, 4, 1, 0, bias=False),
-            nn.BatchNorm2d(ngf * 8),
-            nn.ReLU(True),
-            # state size. ``(ngf*8) x 4 x 4``
-            nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ngf * 4),
-            nn.ReLU(True),
-            # state size. ``(ngf*4) x 8 x 8``
-            nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ngf * 2),
-            nn.ReLU(True),
-            # state size. ``(ngf*2) x 16 x 16``
-            nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ngf),
-            nn.ReLU(True),
-            # state size. ``(ngf) x 32 x 32``
-            nn.ConvTranspose2d(ngf, nc, 4, 2, 1, bias=False),
-            activation or nn.Sigmoid()
-            # state size. ``(nc) x 64 x 64``
+def get_generator(trial):
+    # Create and randomly initialize generator model
+    activations = ["Sigmoid"]
+    activation = trial.suggest_categorical("generator_activation", activations)
+    ngf = 64  # Size of feature maps in generator
+    model = nn.Sequential(
+        # input is Z, going into a convolution
+        nn.ConvTranspose2d(NZ, ngf * 8, 4, 1, 0, bias=False),
+        nn.BatchNorm2d(ngf * 8),
+        nn.ReLU(True),
+        # state size. ``(ngf*8) x 4 x 4``
+        nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
+        nn.BatchNorm2d(ngf * 4),
+        nn.ReLU(True),
+        # state size. ``(ngf*4) x 8 x 8``
+        nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
+        nn.BatchNorm2d(ngf * 2),
+        nn.ReLU(True),
+        # state size. ``(ngf*2) x 16 x 16``
+        nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False),
+        nn.BatchNorm2d(ngf),
+        nn.ReLU(True),
+        # state size. ``(ngf) x 32 x 32``
+        nn.ConvTranspose2d(ngf, NC, 4, 2, 1, bias=False),
+        getattr(nn, activation)(),
+        # state size. ``(NC) x 64 x 64``
+    )
+    model.apply(weights_init)
+    return model
+
+
+def get_discriminator(trial):
+    # Create and randomly initialize discriminator model
+    activations = ["Sigmoid"]
+    activation = trial.suggest_categorical("discriminator_activation", activations)
+    leaky_relu_slope = trial.suggest_float(
+        "discriminator_leaky_relu_slope", 0.1, 0.4, step=0.1
+    )
+    ndf = 64  # Size of feature maps in discriminator
+    model = nn.Sequential(
+        # input is ``(NC) x 64 x 64``
+        nn.Conv2d(NC, ndf, 4, 2, 1, bias=False),
+        nn.LeakyReLU(leaky_relu_slope, inplace=True),
+        # state size. ``(ndf) x 32 x 32``
+        nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
+        nn.BatchNorm2d(ndf * 2),
+        nn.LeakyReLU(leaky_relu_slope, inplace=True),
+        # state size. ``(ndf*2) x 16 x 16``
+        nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
+        nn.BatchNorm2d(ndf * 4),
+        nn.LeakyReLU(leaky_relu_slope, inplace=True),
+        # state size. ``(ndf*4) x 8 x 8``
+        nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
+        nn.BatchNorm2d(ndf * 8),
+        nn.LeakyReLU(leaky_relu_slope, inplace=True),
+        # state size. ``(ndf*8) x 4 x 4``
+        nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
+        getattr(nn, activation)(),
+    )
+    model.apply(weights_init)
+    return model
+
+
+def test_hpo(pytorch_optuna_client):
+    """Run example of running PyTorch and Optuna together on Dask"""
+
+    def objective(trial):
+        dataset = FakeImageDataset()
+
+        # Create the dataloader
+        # NOTE: IMPORTANT w/ optuna; it launches a daemonic process, so PyTorch can't itself use it then.
+        dataloader = torch.utils.data.DataLoader(
+            dataset, batch_size=64, shuffle=True, num_workers=0
         )
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-    def forward(self, input):
-        return self.main(input)
+        # Create models
+        generator = get_generator(trial).to(device)
+        discriminator = get_discriminator(trial).to(device)
 
-    @classmethod
-    def from_trial(cls, trial):
-        activations = [nn.Sigmoid]
-        idx = trial.suggest_categorical(
-            "generator_activation", list(range(len(activations)))
+        # Establish convention for real and fake labels during training
+        real_label = 1.0
+        fake_label = 0.0
+
+        # Setup loss criterion / optimizers and train
+        loss = nn.BCELoss()
+        optimizer_discriminator = optim.Adam(
+            discriminator.parameters(), lr=0.00001, betas=(0.5, 0.999)
         )
-        activation = activations[idx]()
-        return cls(ngpu, activation)
-
-
-class Discriminator(nn.Module):
-    def __init__(self, ngpu, leaky_relu_slope=0.2, activation=None):
-        super(Discriminator, self).__init__()
-        self.ngpu = ngpu
-        self.main = nn.Sequential(
-            # input is ``(nc) x 64 x 64``
-            nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
-            nn.LeakyReLU(leaky_relu_slope, inplace=True),
-            # state size. ``(ndf) x 32 x 32``
-            nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 2),
-            nn.LeakyReLU(leaky_relu_slope, inplace=True),
-            # state size. ``(ndf*2) x 16 x 16``
-            nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 4),
-            nn.LeakyReLU(leaky_relu_slope, inplace=True),
-            # state size. ``(ndf*4) x 8 x 8``
-            nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 8),
-            nn.LeakyReLU(leaky_relu_slope, inplace=True),
-            # state size. ``(ndf*8) x 4 x 4``
-            nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
-            activation or nn.Sigmoid(),
+        optimizer_generator = optim.Adam(
+            generator.parameters(), lr=0.00001, betas=(0.5, 0.999)
         )
+        num_epochs = 5
+        for epoch in range(num_epochs):
+            # For each batch in the dataloader
+            for batch in dataloader:
+                # Train discriminator with all-real batch (maximize log(D(x)) + log(1 - D(G(z))))
+                discriminator.zero_grad()
+                # Format data batch
+                real_cpu = batch[0].to(device)
+                batch_size = real_cpu.size(0)
+                label = torch.full([batch_size], real_label, device=device)
+                # Forward pass real batch through D
+                output = discriminator(real_cpu).view(-1)
+                loss_real = loss(output, label)
+                loss_real.backward()
 
-    def forward(self, input):
-        return self.main(input)
+                # Train with all-fake batch
+                # Generate fake image batch with G
+                noise = torch.randn(batch_size, NZ, 1, 1, device=device)
+                fake = generator(noise)
+                label.fill_(fake_label)
 
-    @classmethod
-    def from_trial(cls, trial):
-        activations = [nn.Sigmoid]
-        idx = trial.suggest_categorical(
-            "descriminator_activation", list(range(len(activations)))
-        )
-        activation = activations[idx]()
+                # Classify all fake batch with D
+                output = discriminator(fake.detach()).view(-1)
+                loss_fake = loss(output, label)
+                loss_fake.backward()
 
-        slopes = np.arange(0.1, 0.4, 0.1)
-        idx = trial.suggest_categorical(
-            "descriminator_leaky_relu_slope", list(range(len(slopes)))
-        )
-        leaky_relu_slope = slopes[idx]
-        return cls(ngpu, leaky_relu_slope, activation)
+                # Compute error of discriminator as sum over the fake and the real batches
+                loss_discriminator = loss_real + loss_fake
+                optimizer_discriminator.step()
+
+                # Update generator: maximize log(D(G(z)))
+                generator.zero_grad()
+                # fake labels are real for generator cost
+                label.fill_(real_label)
+                # Since we just updated D, perform another forward pass of all-fake batch through D
+                output = discriminator(fake).view(-1)
+                loss_generator = loss(output, label)
+                loss_generator.backward()
+
+                # Update G
+                optimizer_generator.step()
+
+            # Handle pruning based on the intermediate value
+            score = loss_discriminator.item()
+            trial.report(score, epoch)
+            if trial.should_prune():
+                raise optuna.exceptions.TrialPruned()
+
+        return score
+
+    study = optuna.create_study(
+        storage=optuna.integration.DaskStorage(), direction="minimize"
+    )
+
+    n_trials = 500
+    futures = [
+        pytorch_optuna_client.submit(study.optimize, objective, n_trials=1, pure=False)
+        for _ in range(n_trials)
+    ]
+    wait(futures)
+
+    assert len(study.trials) == n_trials
