452452391846fbc01dd023acc344b39a6f5ba5e8
James Bourbeau <jrbourbeau@gmail.com>
2023-04-24 14:59:56 -0500

    Add GIL metric

--- tests/conftest.py
+++ tests/conftest.py
@@ -88,7 +88,12 @@ def get_coiled_runtime_version():
             return "unknown"
 
 
-dask.config.set({"coiled.account": "dask-benchmarks"})
+dask.config.set(
+    {
+        "coiled.account": "dask-benchmarks",
+        "distributed.admin.system-monitor.gil.enabled": True,
+    }
+)
 
 COILED_RUNTIME_VERSION = get_coiled_runtime_version()
 COILED_SOFTWARE_NAME = "package_sync"
