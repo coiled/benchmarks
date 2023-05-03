f19ed87ddf41b3065e6f76551c49c40c2e5dd471
crusaderky <crusaderky@gmail.com>
2023-05-03 21:13:31 +0100

    guido/python38

--- setup.py
+++ setup.py
@@ -55,7 +55,6 @@ setup(
     license="BSD",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: BSD License",
