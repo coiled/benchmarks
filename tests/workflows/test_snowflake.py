f4e5f1777f1b6262c8cf2548d531c3b1940d36b7
Irina Truong <i.chernyavska@gmail.com>
2023-05-03 12:12:01 -0700

    Read and explore data.

--- tests/workflows/test_snowflake.py
+++ tests/workflows/test_snowflake.py
@@ -1,5 +1,6 @@
 import os
 import urllib
+import uuid
 import zipfile
 
 import dask.dataframe as dd
@@ -12,12 +13,51 @@ sqlalchemy = pytest.importorskip("sqlalchemy")
 dask_snowflake = pytest.importorskip("dask_snowflake")
 
 from dask_snowflake import read_snowflake, to_snowflake  # noqa: E402
-from snowflake_sqlalchemy import URL  # noqa: E402
+from snowflake.sqlalchemy import URL  # noqa: E402
 from sqlalchemy import create_engine  # noqa: E402
 
 
+@pytest.fixture(scope="module")
+def connection_kwargs():
+    return {
+        "user": os.environ["SNOWFLAKE_USER"],
+        "password": os.environ["SNOWFLAKE_PASSWORD"],
+        "account": os.environ["SNOWFLAKE_ACCOUNT"],
+        "warehouse": os.environ["SNOWFLAKE_WAREHOUSE"],
+        "role": os.environ.get("SNOWFLAKE_ROLE", "public"),
+        "database": os.environ.get("SNOWFLAKE_DATABASE", "testdb"),
+        "schema": os.environ.get("SNOWFLAKE_SCHEMA", "public"),
+    }
+
+
+@pytest.fixture
+def table(connection_kwargs):
+    """Connect to snowflake and create table"""
+    name = f"test_table_{uuid.uuid4().hex}"
+    engine = create_engine(URL(**connection_kwargs))
+    engine.execute(f"DROP TABLE IF EXISTS {name}")
+    engine.execute(
+        f"""create table if not exists {name} (
+                ride_id varchar not null unique,
+                rideable_type varchar not null,
+                started_at timestamp not null,
+                ended_at timestamp not null,
+                start_station_name varchar not null,
+                start_station_id smallint not null,
+                end_station_name varchar not null,
+                end_station_id smallint not null,
+                start_lat number,
+                start_lng number,
+                end_lat number,
+                end_lng number,
+                is_member boolean not null
+            )"""
+    )
+    yield name
+
+
 @pytest.mark.client("snowflake")
-def test_exploratory_analysis(client):
+def test_exploratory_analysis(client, connection_kwargs, table):
     """Upload NYC bike dataset to Snowflake. Then read
     and explore the data."""
 
@@ -73,44 +113,51 @@ def test_exploratory_analysis(client):
 
     wait(ddf)
 
-    # connect to snowflake and create table
-    conn_kwargs = {
-        "user": os.environ["SNOWFLAKE_USER"],
-        "password": os.environ["SNOWFLAKE_PASSWORD"],
-        "account": os.environ["SNOWFLAKE_ACCOUNT"],
-        "warehouse": os.environ["SNOWFLAKE_WAREHOUSE"],
-        "role": os.environ.get("SNOWFLAKE_ROLE", "public"),
-        "database": os.environ.get("SNOWFLAKE_DATABASE", "testdb"),
-        "schema": os.environ.get("SNOWFLAKE_SCHEMA", "public"),
-    }
-    table_name = "citibike_tripdata"
-    engine = create_engine(URL(**conn_kwargs))
-    engine.execute(f"drop table if exists {table_name}")
-    engine.execute(
-        f"""create table if not exists {table_name} (
-        ride_id varchar not null unique,
-        rideable_type varchar not null,
-        started_at timestamp not null,
-        ended_at timestamp not null,
-        start_station_name varchar not null,
-        start_station_id smallint not null,
-        end_station_name varchar not null,
-        end_station_id smallint not null,
-        start_lat number,
-        start_lng number,
-        end_lat number,
-        end_lng number,
-        is_member boolean not null
-    )"""
-    )
-
     # save data to Snowflake
-    to_snowflake(ddf, name=table_name, connection_kwargs=conn_kwargs)
+    to_snowflake(ddf, name=table, connection_kwargs=connection_kwargs)
 
-    query = f"SELECT * FROM {table_name}"
+    # read the data back
+    ddf_out = read_snowflake(
+        f"SELECT * FROM {table}",
+        connection_kwargs=connection_kwargs,
+        npartitions=input_npartitions,
+    ).rename(
+        columns=str.lower
+    )  # snowflake has all uppercase
 
-    _ = read_snowflake(
-        query, connection_kwargs=conn_kwargs, npartitions=input_npartitions
+    ddf_out["is_roundtrip"] = ddf_out["start_station_id"] == ddf_out["end_station_id"]
+
+    # compute counts of roundtrips vs non-roundtrips
+    roundtrips = ddf_out.is_roundtrip.value_counts().compute()  # noqa: F841
+
+    """
+    # maybe plot roundtrips vs one-way
+    import seaborn as sns
+    sns.barplot(roundtrips, x="is_roundtrip", y="count")
+    """
+
+    # explore trip duration
+    ddf_out["trip_duration"] = ddf_out["ended_at"] - ddf_out["started_at"]
+    ddf_out["trip_minutes"] = ddf_out["trip_duration"].apply(
+        lambda x: x.total_seconds() // 60, meta=("trip_duration", int)
     )
 
-    # TODO
+    # find quantiles, then filter out some outliers
+    duration_quantile = ddf_out["trip_minutes"].quantile([0.05, 0.95]).compute()
+    min_duration, max_duration = duration_quantile[0.05], duration_quantile[0.95]
+    ddf_out = ddf_out[
+        (ddf_out.trip_duration_minutes >= min_duration)
+        & (ddf_out.trip_duration_minutes <= max_duration)
+    ]
+
+    durations = (  # noqa: F841
+        ddf_out.trip_duration_minutes.value_counts()
+        .compute()
+        .reset_index(name="count")
+        .rename(columns={"index": "trip_minutes"})
+    )
+
+    """
+    # maybe plot the counts
+    sns.barplot(durations, x="trip_minutes", y="count")
+    """
