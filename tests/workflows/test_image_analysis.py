214f7948445767f3b7459dcbb49cac9b9fef9d94
James Bourbeau <jrbourbeau@gmail.com>
2023-05-04 10:42:55 -0500

    Add context

--- tests/workflows/test_image_analysis.py
+++ tests/workflows/test_image_analysis.py
@@ -5,12 +5,16 @@ from dask_image import ndfilters, ndmeasure, ndmorph
 
 
 def test_BBBC039(small_client):
+    # Based off of https://github.com/GenevieveBuckley/dask-image-talk-2020
     images = da.from_zarr(
         "s3://coiled-datasets/BBBC039", storage_options={"anon": True}
     )
     smoothed = ndfilters.gaussian_filter(images, sigma=[0, 1, 1])
     thresh = ndfilters.threshold_local(smoothed, block_size=images.chunksize)
     threshold_images = smoothed > thresh
+    # Since this image stack appears to be 3-dimensional,
+    # we sandwich a 2d structuring element in between zeros
+    # so that each 2d image slice has the binary closing applied independently
     structuring_element = np.array(
         [
             [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
@@ -25,6 +29,9 @@ def test_BBBC039(small_client):
     index = np.arange(num_features)
     # FIXME: Only selecting the first few images due to cluster idle timeout.
     # Maybe sending large graph? Need to investigate a bit.
-    area = ndmeasure.area(images[:3], label_images[:3], index)
-    mean_intensity = ndmeasure.mean(images[:3], label_images[:3], index)
+    num_images = 10
+    area = ndmeasure.area(images[:num_images], label_images[:num_images], index)
+    mean_intensity = ndmeasure.mean(
+        images[:num_images], label_images[:num_images], index
+    )
     dask.compute(mean_intensity, area)
