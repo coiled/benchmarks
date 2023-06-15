3f9bfb1615bfd23b306a982574baf58e5422f6ee
Irina Truong <i.chernyavska@gmail.com>
2023-06-15 14:00:31 -0700

    Add split_out.

--- tests/workflows/test_imbalanced_join.py
+++ tests/workflows/test_imbalanced_join.py
@@ -7,17 +7,27 @@ import pytest
 
 
 @pytest.mark.client("imbalanced_join")
-def test_merge(client):
+def test_merge(client, shuffle_method):
     """Merge large df and small df"""
     large_df = dd.read_parquet("s3://test-imbalanced-join/df1/")
     small_df = dd.read_parquet("s3://test-imbalanced-join/df2/")
 
-    group_cols = ["df2_group", "bucket", "group1", "group2", "group3", "group4"]
+    # this dataframe has known divisions, use those
+    # to ensure the data is partitioned as expected
+    divisions = list(range(1, 40002, 10))
+    large_df = large_df.set_index("bucket", drop=False, divisions=divisions)
+    split_out = 4000  # same as number of partitions
 
+    group_cols = ["df2_group", "bucket", "group1", "group2", "group3", "group4"]
     res = large_df.merge(
         right=small_df, how="inner", on=["key", "key"], suffixes=["_l", "_r"]
     )[group_cols + ["value"]]
 
-    res = res.groupby(group_cols, sort=False).agg({"value": "sum"})
-    # evaluate the whole merged dataframe
-    res["value"].sum().compute()
+    # group and aggregate, use split_out so that the final data
+    # chunks don't end up aggregating on a single worker
+    (
+        res.groupby(group_cols, sort=False)
+        .agg({"value": "sum"}, split_out=split_out, shuffle=shuffle_method)
+        .value.sum()
+        .compute()
+    )
