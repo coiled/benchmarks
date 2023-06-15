1f134a50b2f1cdd60a876f713c1d6bd242cf2ce8
Irina Truong <i.chernyavska@gmail.com>
2023-06-15 15:55:44 -0700

    Move dataframes into fixture.

--- tests/workflows/test_imbalanced_join.py
+++ tests/workflows/test_imbalanced_join.py
@@ -2,21 +2,31 @@
 This data represents a skewed but realistic dataset that dask has been struggling with in the past.
 Workflow based on https://github.com/coiled/imbalanced-join/blob/main/test_big_join_synthetic.ipynb
 """
+from functools import partial
+
 import dask.dataframe as dd
 import pytest
+from distributed import wait
 
 
 @pytest.mark.client("imbalanced_join")
-def test_merge(client, shuffle_method):
-    """Merge large df and small df"""
+@pytest.fixture
+def dataframes(client):
     large_df = dd.read_parquet("s3://test-imbalanced-join/df1/")
-    small_df = dd.read_parquet("s3://test-imbalanced-join/df2/")
-
-    # this dataframe has known divisions, use those
+    small_df = dd.read_parquet("s3://test-imbalanced-join/df2/").persist()
+    # large dataframe has known divisions, use those
     # to ensure the data is partitioned as expected
     divisions = list(range(1, 40002, 10))
-    large_df = large_df.set_index("bucket", drop=False, divisions=divisions)
-    split_out = 4000  # same as number of partitions
+    large_df = large_df.set_index("bucket", drop=False, divisions=divisions).persist()
+    wait(large_df, client, 10 * 60)
+    wait(small_df, client, 10 * 60)
+    yield large_df, small_df
+
+
+@pytest.mark.client("imbalanced_join")
+def test_merge(dataframes, shuffle_method):
+    """Merge large df and small df"""
+    large_df, small_df = dataframes
 
     group_cols = ["df2_group", "bucket", "group1", "group2", "group3", "group4"]
     res = large_df.merge(
@@ -25,9 +35,18 @@ def test_merge(client, shuffle_method):
 
     # group and aggregate, use split_out so that the final data
     # chunks don't end up aggregating on a single worker
-    (
-        res.groupby(group_cols, sort=False)
-        .agg({"value": "sum"}, split_out=split_out, shuffle=shuffle_method)
-        .value.sum()
-        .compute()
-    )
+    # TODO: workers are still getting killed, even with split_out
+    # (
+    #     res.groupby(group_cols, sort=False)
+    #     .agg({"value": "sum"}, split_out=4000, shuffle=shuffle_method)
+    #     .value.sum()
+    #     .compute()
+    # )
+
+    def aggregate_partition(part, *, shuffle=None):
+        return part.groupby(group_cols, sort=False).agg(
+            {"value": "sum"}, shuffle=shuffle, split_out=100
+        )
+
+    shuffle_aggregate = partial(aggregate_partition, shuffle=shuffle_method)
+    res.map_partitions(shuffle_aggregate).value.sum().compute()
