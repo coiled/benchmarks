e6899695cacd65e18ef12fa6f73520c8b43bc748
Irina Truong <i.chernyavska@gmail.com>
2023-07-21 14:03:07 -0700

    Simple deltalake benchmark.

---
+++ tests/benchmarks/test_deltalake.py
@@ -0,0 +1,34 @@
+import dask.dataframe as dd
+import dask_deltatable as ddt
+import pytest
+
+
+@pytest.fixture(params=["read_deltalake", "read_parquet"])
+def ddf(request, small_client):
+    uri = "s3://coiled-datasets/delta/ds20f_100M/"
+    if request.param == "read_deltalake":
+        yield ddt.read_deltalake(uri)
+    else:
+        yield dd.read_parquet(f"{uri}*.parquet", engine="pyarrow")
+
+
+def test_column_agg(ddf):
+    ddf["float1"].agg(["sum", "mean"]).compute()
+
+
+def test_group_agg(ddf):
+    ddf = ddf[["int1", "int2", "int3"]]
+    (
+        ddf.groupby(["int2", "int3"], dropna=False, observed=True)
+        .agg({"int1": ["sum", "mean"]})
+        .compute()
+    )
+
+
+def test_group_median(ddf, shuffle_method):
+    ddf = ddf[["int1", "int2", "int3"]]
+    (
+        ddf.groupby(["int2", "int3"], dropna=False, observed=True)
+        .agg({"int1": ["median", "std"]}, shuffle=shuffle_method)
+        .compute()
+    )
