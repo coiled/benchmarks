57c1b25130aa8c2c2114fa31eec035077fe268a4
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2023-08-21 21:16:03 +0200

    Fix

--- tests/benchmarks/test_join.py
+++ tests/benchmarks/test_join.py
@@ -21,7 +21,7 @@ def test_join_big(small_client, memory_multiplier, configure_shuffling):
     df2_big["predicate"] = df2_big["0"] * 1e9
     df2_big = df2_big.astype({"predicate": "int"})
 
-    join = df1_big.merge(df2_big, on="predicate", how="inner")
+    join = df1_big.merge(df2_big, on="predicate", how="inner").map_partitions(lambda x: x)
     result = join.size
     wait(result, small_client, 20 * 60)
 
@@ -47,7 +47,7 @@ def test_join_big_small(small_client, memory_multiplier, configure_shuffling):
     df_small["predicate"] = df_small["0"] * 1e9
     df_small_pd = df_small.astype({"predicate": "int"}).compute()
 
-    join = df_big.merge(df_small_pd, on="predicate", how="inner")
+    join = df_big.merge(df_small_pd, on="predicate", how="inner").map_partitions(lambda x: x)
     result = join.size
     wait(result, small_client, 20 * 60)
 
