999a43ab70b6fcae7eb40eaa3954615385897d47
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2023-09-02 01:28:29 +0200

    Only query 4

--- tests/benchmarks/test_tpch.py
+++ tests/benchmarks/test_tpch.py
@@ -142,7 +142,6 @@ def read_data(filename):
 #     ]
 
 
-@pytest.mark.client("tpch")
 def test_query_4(tpch_client):
     date1 = datetime.strptime("1993-10-01", "%Y-%m-%d")
     date2 = datetime.strptime("1993-07-01", "%Y-%m-%d")
