77ccd106e40e478455d2d1d9bf182887c57a4694
fjetter <fjetter@users.noreply.github.com>
2023-10-19 15:42:38 +0200

    add DB migration

---
+++ alembic/versions/78c6e00fee88_remove_task_based_shuffle.py
@@ -0,0 +1,46 @@
+"""Remove task based shuffle
+
+Revision ID: 78c6e00fee88
+Revises: 778e617a2886
+Create Date: 2023-10-19 15:26:04.281985
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "78c6e00fee88"
+down_revision = "778e617a2886"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.execute(
+        """
+        delete from test_run
+        where (
+               originalname in ('test_shuffle')
+               or name in ('test_join_big[1-tasks]', 'test_join_big[0.1-tasks]')
+            )
+        """
+    )
+    op.execute(
+        """
+        update test_run
+        set name = 'test_join_big[1]'
+        where name == 'test_join_big[1-p2p]';
+        """
+    )
+    op.execute(
+        """
+        update test_run
+        set name = 'test_join_big[0.1]'
+        where name == 'test_join_big[0.1-p2p]';
+        """
+    )
+
+
+def downgrade() -> None:
+    pass
