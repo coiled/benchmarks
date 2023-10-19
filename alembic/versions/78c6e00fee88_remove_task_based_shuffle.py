3857e53b6bf94805207765ff257c89a1404eedce
fjetter <fjetter@users.noreply.github.com>
2023-10-19 15:56:32 +0200

    add test_set_index to db migration as well

--- alembic/versions/78c6e00fee88_remove_task_based_shuffle.py
+++ alembic/versions/78c6e00fee88_remove_task_based_shuffle.py
@@ -22,7 +22,8 @@ def upgrade() -> None:
         delete from test_run
         where (
                originalname in ('test_shuffle', 'test_cluster_reconnect')
-               or name in ('test_join_big[1-tasks]', 'test_join_big[0.1-tasks]')
+               or name like 'test_join_big[%tasks%]'
+               or name like 'test_set_index[%tasks%]'
             )
         """
     )
@@ -40,6 +41,15 @@ def upgrade() -> None:
         where name == 'test_join_big[0.1-p2p]';
         """
     )
+    for b in [True, False]:
+        for factor in [0.1, 1]:
+            op.execute(
+            f"""
+                update test_run
+                set name = 'test_set_index[{factor}-{b}]'
+                where name == 'test_set_index[{factor}-p2p-{b}]';
+                """
+            )
 
 
 def downgrade() -> None:
