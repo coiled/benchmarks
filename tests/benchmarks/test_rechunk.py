73d34d798dbee0c7f5bdb305c84e3a171723c162
Hendrik Makait <hendrik@coiled.io>
2023-10-20 12:08:46 +0200

    pre-commit

--- tests/benchmarks/test_rechunk.py
+++ tests/benchmarks/test_rechunk.py
@@ -1,20 +1,22 @@
 from __future__ import annotations
 
+import dask
 import dask.array as da
 import pytest
 
-from typing import Any
-
-import dask
 from ..utils_test import cluster_memory, scaled_array_shape
+
 
 @pytest.fixture(
     params=[
-        pytest.param("8 MiB", marks=pytest.mark.skip(
-            "FIXME: Skip for scheduled benchmarks due to runtime, uncomment on demand "
-            "or once we have a more flexible system for running tests on different schedules."
-        )),
-        "128 MiB"
+        pytest.param(
+            "8 MiB",
+            marks=pytest.mark.skip(
+                "FIXME: Skip for scheduled benchmarks due to runtime, uncomment on demand "
+                "or once we have a more flexible system for running tests on different schedules."
+            ),
+        ),
+        "128 MiB",
     ]
 )
 def chunksize(request):
@@ -26,7 +28,10 @@ def configure_chunksize(chunksize):
     with dask.config.set({"array.chunk-size": chunksize}):
         yield
 
-def test_tiles_to_rows(small_client, memory_multiplier, configure_chunksize, configure_rechunking):
+
+def test_tiles_to_rows(
+    small_client, memory_multiplier, configure_chunksize, configure_rechunking
+):
     memory = cluster_memory(small_client)
     shape = scaled_array_shape(memory * memory_multiplier, ("x", "x"))
 
@@ -34,7 +39,9 @@ def test_tiles_to_rows(small_client, memory_multiplier, configure_chunksize, con
     arr.rechunk((-1, "auto")).sum().compute()
 
 
-def test_swap_axes(small_client, memory_multiplier, configure_chunksize, configure_rechunking):
+def test_swap_axes(
+    small_client, memory_multiplier, configure_chunksize, configure_rechunking
+):
     memory = cluster_memory(small_client)
     shape = scaled_array_shape(memory * memory_multiplier, ("x", "x"))
 
