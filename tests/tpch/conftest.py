01f3d6924a1ed410ca1eb6fd805d352ffd051185
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2023-10-26 15:53:25 +0200

    Hotfix tpch related failures (#1149)

--- tests/tpch/conftest.py
+++ tests/tpch/conftest.py
@@ -291,19 +291,21 @@ def fs(local):
     if local:
         return None
     else:
-        import boto3
-        from pyarrow.fs import S3FileSystem
-
-        session = boto3.session.Session()
-        credentials = session.get_credentials()
-
-        fs = S3FileSystem(
-            secret_key=credentials.secret_key,
-            access_key=credentials.access_key,
-            region="us-east-2",
-            session_token=credentials.token,
-        )
-        return fs
+        return None
+        # TODO: Add this when arrow fs is supported
+        # import boto3
+        # from pyarrow.fs import S3FileSystem
+        #
+        # session = boto3.session.Session()
+        # credentials = session.get_credentials()
+        #
+        # fs = S3FileSystem(
+        #     secret_key=credentials.secret_key,
+        #     access_key=credentials.access_key,
+        #     region="us-east-2",
+        #     session_token=credentials.token,
+        # )
+        # return fs
 
 
 #################################################
