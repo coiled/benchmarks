8c305c9e0116b4d792acff6f8ce5f2301e3a48e5
Miles Granger <miles59923@gmail.com>
2023-10-31 12:26:04 +0100

    Add pyspark query 8 [skip ci]

--- tests/tpch/test_pyspark.py
+++ tests/tpch/test_pyspark.py
@@ -265,6 +265,63 @@ def test_query_7(spark, dataset_path):
     spark.sql(query).show()
 
 
+def test_query_8(spark, dataset_path):
+    for name in (
+        "part",
+        "supplier",
+        "lineitem",
+        "orders",
+        "customer",
+        "nation",
+        "region",
+    ):
+        register_table(spark, dataset_path, name)
+
+    query = """
+    select
+            supp_nation,
+            cust_nation,
+            l_year,
+            sum(volume) as revenue
+        from
+            (
+                select
+                    n1.n_name as supp_nation,
+                    n2.n_name as cust_nation,
+                    year(l_shipdate) as l_year,
+                    l_extendedprice * (1 - l_discount) as volume
+                from
+                    supplier,
+                    lineitem,
+                    orders,
+                    customer,
+                    nation n1,
+                    nation n2
+                where
+                    s_suppkey = l_suppkey
+                    and o_orderkey = l_orderkey
+                    and c_custkey = o_custkey
+                    and s_nationkey = n1.n_nationkey
+                    and c_nationkey = n2.n_nationkey
+                    and (
+                        (n1.n_name = 'FRANCE' and n2.n_name = 'GERMANY')
+                        or (n1.n_name = 'GERMANY' and n2.n_name = 'FRANCE')
+                    )
+                    and l_shipdate between date '1995-01-01' and date '1996-12-31'
+            ) as shipping
+        group by
+            supp_nation,
+            cust_nation,
+            l_year
+        order by
+            supp_nation,
+            cust_nation,
+            l_year
+    """
+
+    spark.sql(query).show()
+
+
 def fix_timestamp_ns_columns(query):
     """
     scale100 stores l_shipdate/o_orderdate as timestamp[us]
