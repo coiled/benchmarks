65220763ef96bba56045b64e1e51518394c70ef3
Hendrik Makait <hendrik@makait.com>
2024-02-05 20:50:57 +0100

    utils.py

---
+++ tests/tpch/utils.py
@@ -0,0 +1,18 @@
+def get_dataset_path(local, scale):
+    remote_paths = {
+        1: "s3://coiled-runtime-ci/tpc-h/snappy/scale-1/",
+        10: "s3://coiled-runtime-ci/tpc-h/snappy/scale-10/",
+        100: "s3://coiled-runtime-ci/tpc-h/snappy/scale-100/",
+        1000: "s3://coiled-runtime-ci/tpc-h/snappy/scale-1000/",
+        10000: "s3://coiled-runtime-ci/tpc-h/snappy/scale-10000/",
+    }
+    local_paths = {
+        1: "./tpch-data/scale-1/",
+        10: "./tpch-data/scale-10/",
+        100: "./tpch-data/scale-100/",
+    }
+
+    if local:
+        return local_paths[scale]
+    else:
+        return remote_paths[scale]
