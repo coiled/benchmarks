d97d0c2df3c74bd6d055572a94970fe3599b529b
Hendrik Makait <hendrik@makait.com>
2024-02-05 20:33:24 +0100

    Use Coiled cluster on CI

--- tests/tpch/test_correctness.py
+++ tests/tpch/test_correctness.py
@@ -1,11 +1,14 @@
+import os
 import pathlib
 
+import coiled
+import dask
 import pandas as pd
 import pytest
-from distributed import LocalCluster
+from distributed import LocalCluster, performance_report
 
 from .generate_answers import generate as generate_answers
-from .generate_data import generate as generate_data
+from .utils import get_dataset_path
 
 VERIFICATION_SCALE = 1
 
@@ -19,26 +22,62 @@ def answers_path(tmp_path_factory):
 
 
 @pytest.fixture(scope="module")
-def data_path(tmp_path_factory):
-    path = tmp_path_factory.mktemp("data")
-    scale = VERIFICATION_SCALE
-    return pathlib.Path(generate_data(scale=scale, path=str(path), relaxed_schema=True))
-
-
-@pytest.fixture(scope="module")
-def cluster():
-    with LocalCluster() as cluster:
-        yield cluster
+def cluster(
+    local,
+    module,
+    dask_env_variables,
+    github_cluster_tags,
+    name,
+):
+    if local:
+        with LocalCluster() as cluster:
+            yield cluster
+    else:
+        with dask.config.set({"distributed.scheduler.worker-saturation": "inf"}):
+            with coiled.Cluster(
+                name=f"tpch-{module}-{VERIFICATION_SCALE}-{name}",
+                environ=dask_env_variables,
+                tags=github_cluster_tags,
+                region="us-east-2",
+                worker_vm_types=["m6i.large"],  # 2 CPUs, 8GiB RAM
+                n_workers=2,
+                wait_for_workers=True,
+                scheduler_vm_types=["m6i.large"],
+                spot_policy="spot_with_fallback",
+            ) as cluster:
+                yield cluster
 
 
 @pytest.fixture
-def client(cluster, restart):
+def client(
+    request,
+    cluster,
+    testrun_uid,
+    cluster_kwargs,
+    benchmark_time,
+    restart,
+    local,
+    query,
+):
     with cluster.get_client() as client:
         if restart:
             client.restart()
         client.run(lambda: None)
 
-        yield client
+        local = "local" if local else "cloud"
+        if request.config.getoption("--performance-report"):
+            if not os.path.exists("performance-reports"):
+                os.mkdir("performance-reports")
+            with performance_report(
+                filename=os.path.join(
+                    "performance-reports", f"{local}-{VERIFICATION_SCALE}-{query}.html"
+                )
+            ):
+                with benchmark_time:
+                    yield client
+        else:
+            with benchmark_time:
+                yield client
 
 
 def get_expected_answer(query: int, answer_dir: pathlib.Path):
@@ -96,9 +135,9 @@ def verify_result(result: pd.DataFrame, query: int, answer_dir: pathlib.Path):
         22,
     ],
 )
-def test_dask_results(query, client, answers_path, data_path):
+def test_dask_results(query, local, answers_path, client):
     from . import dask_queries
 
     func = getattr(dask_queries, f"query_{query}")
-    result = func(str(data_path) + "/", None).compute()
+    result = func(get_dataset_path(local, VERIFICATION_SCALE), None).compute()
     verify_result(result, query, answers_path)
