e53ff48d3db8b8a0b27e066fc98da55718b2845f
Hendrik Makait <hendrik@makait.com>
2024-03-19 14:55:42 +0100

    Fix memory issues for Spark

--- tests/tpch/conftest.py
+++ tests/tpch/conftest.py
@@ -250,8 +250,7 @@ def spark_setup(cluster, local):
         )
         spark_dashboard = parse_url("http://localhost:4040")
     else:
-        spark = cluster.get_spark()
-
+        spark = cluster.get_spark(executor_memory_factor=0.8, worker_memory_factor=0.9)
         # Available on coiled>=1.12.4
         if not hasattr(cluster, "_spark_dashboard"):
             cluster._spark_dashboard = (
