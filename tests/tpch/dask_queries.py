3187f3b69bf5808ea581db2aca278fa6b8bce7c5
Hendrik Makait <hendrik@makait.com>
2024-03-21 15:00:57 +0100

    Add split_out

--- tests/tpch/dask_queries.py
+++ tests/tpch/dask_queries.py
@@ -791,7 +791,7 @@ def query_15(dataset_path, fs, scale):
     lineitem["revenue"] = lineitem.l_extendedprice * (1 - lineitem.l_discount)
     revenue = (
         lineitem.groupby("l_suppkey")
-        .revenue.sum()
+        .revenue.sum(split_out=True)
         .to_frame()
         .reset_index()
         .rename(columns={"revenue": "total_revenue", "l_suppkey": "supplier_no"})
