6fab5ea3219ba94f3c9fb039e71911cffe9f3601
James Bourbeau <jrbourbeau@gmail.com>
2024-08-30 10:34:47 -0500

    Add own workflow

--- tests/geospatial/test_rechunking.py
+++ tests/geospatial/test_rechunking.py
@@ -3,18 +3,20 @@ import xarray as xr
 from coiled.credentials.google import CoiledShippedCredentials
 
 
+@pytest.mark.timeout(5400)  # 90 minutes
 @pytest.mark.client("era5_rechunking")
-def test_era5_rechunking(client, gcs_url):
+def test_era5_rechunking(client, gcs_url, scale):
     # Load dataset
     ds = xr.open_zarr(
         "gs://weatherbench2/datasets/era5/1959-2023_01_10-full_37-1h-0p25deg-chunk-1.zarr",
     ).drop_encoding()
 
-    time_range = slice("2020-01-01", "2023-01-01")  # 101.83 GiB (small)
-    # time_range = slice("2013-01-01", "2023-01-01")      # 339.09 GiB
-    # time_range = slice("2003-01-01", "2023-01-01")      # 678.19 GiB (medium?)
-    # time_range = slice("1990-01-01", "2023-01-01")      # 1.09 TiB
-    # time_range = slice(None)                            # 2.12 TiB (large?)
+    if scale == "small":
+        # 101.83 GiB (small)
+        time_range = slice("2020-01-01", "2023-01-01")
+    else:
+        # 2.12 TiB (large)
+        time_range = slice(None)
     subset = ds.sea_surface_temperature.sel(time=time_range)
 
     # Rechunk
