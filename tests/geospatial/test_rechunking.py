fac49a2fc88c1f5c55487617bc38591ffa03bbd0
James Bourbeau <jrbourbeau@gmail.com>
2024-09-12 13:13:20 -0500

    Add zonal averaging workflow

--- tests/geospatial/test_rechunking.py
+++ tests/geospatial/test_rechunking.py
@@ -1,25 +1,39 @@
-import pytest
 import xarray as xr
 from coiled.credentials.google import CoiledShippedCredentials
 
 
-@pytest.mark.client("era5_rechunking")
-def test_era5_rechunking(client, gcs_url, scale):
-    # Load dataset
-    ds = xr.open_zarr(
-        "gs://weatherbench2/datasets/era5/1959-2023_01_10-full_37-1h-0p25deg-chunk-1.zarr",
-    ).drop_encoding()
+def test_era5_rechunking(
+    gcs_url,
+    scale,
+    client_factory,
+    cluster_kwargs={
+        "workspace": "dask-engineering-gcp",
+        "region": "us-central1",
+        "wait_for_workers": True,
+    },
+    scale_n_workers={
+        "small": 10,
+        "large": 100,
+    },
+):
+    with client_factory(
+        n_workers=scale_n_workers[scale], **cluster_kwargs
+    ) as client:  # noqa: F841
+        # Load dataset
+        ds = xr.open_zarr(
+            "gs://weatherbench2/datasets/era5/1959-2023_01_10-full_37-1h-0p25deg-chunk-1.zarr",
+        ).drop_encoding()
 
-    if scale == "small":
-        # 101.83 GiB (small)
-        time_range = slice("2020-01-01", "2023-01-01")
-    else:
-        # 2.12 TiB (large)
-        time_range = slice(None)
-    subset = ds.sea_surface_temperature.sel(time=time_range)
+        if scale == "small":
+            # 101.83 GiB (small)
+            time_range = slice("2020-01-01", "2023-01-01")
+        else:
+            # 2.12 TiB (large)
+            time_range = slice(None)
+        subset = ds.sea_surface_temperature.sel(time=time_range)
 
-    # Rechunk
-    result = subset.chunk({"time": -1, "longitude": "auto", "latitude": "auto"})
+        # Rechunk
+        result = subset.chunk({"time": -1, "longitude": "auto", "latitude": "auto"})
 
-    # Write result to cloud storage
-    result.to_zarr(gcs_url, storage_options={"token": CoiledShippedCredentials()})
+        # Write result to cloud storage
+        result.to_zarr(gcs_url, storage_options={"token": CoiledShippedCredentials()})
---
+++ tests/geospatial/test_zonal_average.py
@@ -0,0 +1,73 @@
+"""
+This example was adapted from https://github.com/dcherian/dask-demo/blob/main/nwm-aws.ipynb
+"""
+
+import dask
+import flox.xarray
+import fsspec
+import numpy as np
+import rioxarray
+import xarray as xr
+
+
+def test_nwm(
+    s3_url,
+    s3_factory,
+    scale,
+    client_factory,
+    cluster_kwargs={
+        "workspace": "dask-engineering",
+        "region": "us-east-1",
+        "wait_for_workers": True,
+    },
+    scale_n_workers={
+        "small": 10,
+        "large": 100,
+    },
+):
+    with client_factory(
+        n_workers=scale_n_workers[scale], **cluster_kwargs
+    ) as client:  # noqa: F841
+        ds = xr.open_zarr(
+            fsspec.get_mapper(
+                "s3://noaa-nwm-retrospective-2-1-zarr-pds/rtout.zarr", anon=True
+            ),
+            consolidated=True,
+            chunks={"time": 896, "x": 350, "y": 350},
+        )
+
+        if scale == "small":
+            # 6.03 TiB
+            time_range = slice("2020-01-01", "2020-12-31")
+        else:
+            # 252.30 TiB
+            time_range = slice("1979-02-01", "2020-12-31")
+        subset = ds.zwattablrt.sel(time=time_range)
+
+        fs = s3_factory(requester_pays=True)
+        with dask.annotate(retries=3):
+            counties = rioxarray.open_rasterio(
+                fs.open("s3://nwm-250m-us-counties/Counties_on_250m_grid.tif"),
+                chunks="auto",
+            ).squeeze()
+
+        # Remove any small floating point error in coordinate locations
+        _, counties_aligned = xr.align(subset, counties, join="override")
+
+        counties_aligned = counties_aligned.persist()
+
+        county_id = np.unique(counties_aligned.data).compute()
+        county_id = county_id[county_id != 0]
+        print(f"There are {len(county_id)} counties!")
+
+        county_mean = flox.xarray.xarray_reduce(
+            subset,
+            counties_aligned.rename("county"),
+            func="mean",
+            expected_groups=(county_id,),
+        )
+
+        county_mean.load()
+        yearly_mean = county_mean.mean("time")
+        # Save dataset for further analysis
+        yearly_mean.to_netcdf(f"{s3_url}/mean_zwattablrt_nwm_{scale}.nc")
