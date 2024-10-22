38c370cd6c8853d4caaad4c9547fe0a2feec21e7
Hendrik Makait <hendrik@makait.com>
2024-10-22 18:37:23 +0200

    Fix output chunks

--- tests/geospatial/test_climatology.py
+++ tests/geospatial/test_climatology.py
@@ -158,6 +158,7 @@ def test_highlevel_api(
         # Load dataset
         ds = xr.open_zarr(
             "gs://weatherbench2/datasets/era5/1959-2023_01_10-wb13-6h-1440x721.zarr",
+            chunks={"time": "auto"},
         )
 
         if scale == "small":
@@ -174,6 +175,7 @@ def test_highlevel_api(
             time_range = slice("1959-01-01", "2022-12-31")
             variables = ["sea_surface_temperature", "snow_depth"]
         ds = ds[variables].sel(time=time_range)
+        original_chunks = ds.chunks
 
         # Drop all static variables
         ds = ds.drop_vars([k for k, v in ds.items() if "time" not in v.dims])
@@ -201,8 +203,8 @@ def test_highlevel_api(
         pancake_chunks = {
             "hour": 1,
             "dayofyear": 1,
-            "latitude": ds.chunks["latitude"],
-            "longitude": ds.chunks["longitude"],
+            "latitude": original_chunks["latitude"],
+            "longitude": original_chunks["longitude"],
         }
         result = ds.chunk(pancake_chunks)
         result.to_zarr(gcs_url, storage_options={"token": CoiledShippedCredentials()})
