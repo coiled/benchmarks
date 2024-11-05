d1d776b326bbc7ed3a34dde75292c9649ac7b64f
Hendrik Makait <hendrik@makait.com>
2024-11-05 15:18:47 +0100

    Recalibrate scales of regridding benchmark (#1581)

--- tests/geospatial/test_regridding.py
+++ tests/geospatial/test_regridding.py
@@ -1,15 +1,12 @@
-import pytest
 from coiled.credentials.google import CoiledShippedCredentials
 
 from tests.geospatial.workloads.regridding import xesmf
 
 
-@pytest.mark.parametrize("output_resolution", [1.5, 0.1])
 def test_xesmf(
     gcs_url,
     scale,
     client_factory,
-    output_resolution,
     cluster_kwargs={
         "workspace": "dask-benchmarks-gcp",
         "region": "us-central1",
@@ -17,8 +14,8 @@ def test_xesmf(
     },
     scale_kwargs={
         "small": {"n_workers": 10},
-        "medium": {"n_workers": 100},
-        "large": {"n_workers": 100},
+        "medium": {"n_workers": 10},
+        "large": {"n_workers": 10},
     },
 ):
     with client_factory(
@@ -26,7 +23,6 @@ def test_xesmf(
     ) as client:  # noqa: F841
         result = xesmf(
             scale=scale,
-            output_resolution=output_resolution,
             storage_url=gcs_url,
             storage_options={"token": CoiledShippedCredentials()},
         )
